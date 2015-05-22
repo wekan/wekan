6ef9c7e95fd2c85f944db89a746a8457e216403c
Maxime Quandalle <maxime@quandalle.com>
2015-05-22 20:17:40 +0200

    Improve popup animation
    When going back wait for the popup transition to end before removing
    the DOM element.

--- client/components/main/popup.js
+++ client/components/main/popup.js
@@ -1,3 +1,12 @@
+// XXX This event list should be abstracted somewhere else.
+var endTransitionEvents = [
+  'webkitTransitionEnd',
+  'otransitionend',
+  'oTransitionEnd',
+  'msTransitionEnd',
+  'transitionend'
+].join(' ');
+
 Popup.template.events({
   click: function(evt) {
     if (evt.originalEvent) {
@@ -13,4 +22,19 @@ Popup.template.events({
   'click .js-confirm': function() {
     this.__afterConfirmAction.call(this);
   }
+});
+
+// When a popup content is removed (ie, when the user press the "back" button),
+// we need to wait for the container translation to end before removing the
+// actual DOM element. For that purpose we use the undocumented `_uihooks` API.
+Popup.template.onRendered(function() {
+  var container = this.find('.content-container');
+  container._uihooks = {
+    removeElement: function(node) {
+      $(node).addClass('no-height');
+      $(container).one(endTransitionEvents, function() {
+        node.parentNode.removeChild(node);
+      });
+    }
+  };
 });
