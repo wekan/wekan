wekan/wekan

commit 07cc454b410a2b027960971662757810bd301f5d

Upgrade meteor to 1.2-rc.4 and package versions
The new version of meteor speeds up the reload cycle, which is super
valuable during the development. I also removed the "imply-everything"
"meteor-platform" package in favor of a more fined-grained package
selection.

This version also introduces ES6 support with transparent babeljs
transpilation. Most features are enable (with the notable exception of
ES6 modules) and this commit started to use them in places where a XXX
comment suggested it.

client/components/cards/details.js
@@ -40,11 +40,10 @@ BlazeComponent.extendComponent({
   },
 
   events: function() {
-    // XXX We can't define this event directly in the event map below because we
-    // miss ES6 object keys interpolation.
-    var events = {};
-    events[CSSEvents.animationend + ' .js-card-details'] = function() {
-      this.isLoaded.set(true);
+    var events = {
+      [CSSEvents.animationend + ' .js-card-details']: function() {
+        this.isLoaded.set(true);
+      }
     };
 
     return [_.extend(events, {

client/lib/escapeActions.js
@@ -17,25 +17,20 @@ EscapeActions = {
     'sidebarView'
   ],
 
-  register: function(label, action, condition, options) {
-    condition = condition || function() { return true; };
-    options = options || {};
-
-    // XXX Rewrite this with ES6: .push({ priority, condition, action })
+  register: function(label, action, condition = () => true, options = {}) {
     var priority = this.hierarchy.indexOf(label);
     if (priority === -1) {
       throw Error('You must define the label in the EscapeActions hierarchy');
     }
 
     this._actions.push({
-      priority: priority,
-      condition: condition,
-      action: action,
+      priority,
+      condition,
+      action,
       noClickEscapeOn: options.noClickEscapeOn,
       enabledOnClick: !! options.enabledOnClick
     });
-    // XXX Rewrite this with ES6: => function
-    this._actions = _.sortBy(this._actions, function(a) { return a.priority; });
+    this._actions = _.sortBy(this._actions, (a) => { return a.priority; });
   },
 
   executeLowest: function() {

.meteor/packages
@@ -6,21 +6,17 @@
 # XXX Should we replace tmeasday:presence by 3stack:presence? Or maybe the
 # packages will merge in the future?
 
-meteor-platform
+meteor-base
 
-# Account system
-accounts-password
-kenton:accounts-sandstorm
-service-configuration
-useraccounts:core
-useraccounts:unstyled
-useraccounts:flow-routing
-
-# Compilers
+# Build system
+es5-shim
+ecmascript
+standard-minifiers
 mquandalle:jade
 mquandalle:stylus
 
 # Collections
+mongo
 aldeed:collection2
 cfs:gridfs
 cfs:standard-packages
@@ -30,7 +26,22 @@ matb33:collection-hooks
 matteodem:easy-search
 reywood:publish-composite
 
+# Account system
+accounts-password
+kenton:accounts-sandstorm
+service-configuration
+useraccounts:core
+useraccounts:unstyled
+useraccounts:flow-routing
+
 # Utilities
+check
+jquery
+random
+reactive-dict
+session
+tracker
+underscore
 alethes:pages
 arillo:flow-router-helpers
 audit-argument-checks
@@ -43,19 +54,17 @@ mquandalle:bower
 mquandalle:moment
 ongoworks:speakingurl
 raix:handlebar-helpers
-random
-reactive-dict
 tap:i18n
 tmeasday:presence
-underscore
 
 # UI components
+blaze
+reactive-var
 fortawesome:fontawesome
 mousetrap:mousetrap
 mquandalle:jquery-textcomplete
 mquandalle:jquery-ui-drag-drop-sort
 peerlibrary:blaze-components
 perak:markdown
-reactive-var
 seriousm:emoji-continued
 templates:tabs

.meteor/release
@@ -1,1 +1,1 @@
-METEOR@1.1.0.2
+METEOR@1.2-rc.4

.meteor/versions
@@ -1,17 +1,21 @@
-accounts-base@1.2.0
-accounts-password@1.1.1
+accounts-base@1.2.1-rc.0
+accounts-password@1.1.2-rc.0
 aldeed:collection2@2.3.3
 aldeed:simple-schema@1.3.3
 alethes:pages@1.8.4
-arillo:flow-router-helpers@0.4.3
-audit-argument-checks@1.0.3
-autoupdate@1.2.1
-base64@1.0.3
-binary-heap@1.0.3
-blaze@2.1.2
-blaze-tools@1.0.3
-boilerplate-generator@1.0.3
-callback-hook@1.0.3
+arillo:flow-router-helpers@0.4.4_1
+audit-argument-checks@1.0.4-rc.0
+autoupdate@1.2.3-rc.0
+babel-compiler@5.8.20-rc.0
+babel-runtime@0.1.4-rc.0
+base64@1.0.4-rc.0
+binary-heap@1.0.4-rc.0
+blaze@2.1.3-rc.0
+blaze-tools@1.0.4-rc.0
+boilerplate-generator@1.0.4-rc.0
+caching-compiler@1.0.0-rc.0
+caching-html-compiler@1.0.1-rc.0
+callback-hook@1.0.4-rc.0
 cfs:access-point@0.1.49
 cfs:base-package@0.0.30
 cfs:collection@0.5.5
@@ -29,91 +33,106 @@ cfs:storage-adapter@0.2.2
 cfs:tempstore@0.1.5
 cfs:upload-http@0.0.20
 cfs:worker@0.1.4
-check@1.0.5
-coffeescript@1.0.6
+check@1.0.6-rc.0
+coffeescript@1.0.8-rc.1
 cosmos:browserify@0.5.0
 dburles:collection-helpers@1.0.3
-ddp@1.1.0
-deps@1.0.7
-ejson@1.0.6
-email@1.0.6
-fastclick@1.0.3
+ddp@1.2.1-rc.0
+ddp-client@1.2.1-rc.0
+ddp-common@1.2.1-rc.0
+ddp-rate-limiter@1.0.0-rc.0
+ddp-server@1.2.1-rc.0
+deps@1.0.8-rc.0
+diff-sequence@1.0.1-rc.0
+ecmascript@0.1.3-rc.0
+ecmascript-collections@0.1.5-rc.0
+ejson@1.0.7-rc.0
+email@1.0.7-rc.0
+es5-shim@0.1.0-rc.0
+fastclick@1.0.7-rc.0
 fortawesome:fontawesome@4.4.0
-geojson-utils@1.0.3
-html-tools@1.0.4
-htmljs@1.0.4
-http@1.1.0
-id-map@1.0.3
+geojson-utils@1.0.4-rc.0
+hot-code-push@1.0.0-rc.0
+html-tools@1.0.5-rc.0
+htmljs@1.0.5-rc.1
+http@1.1.1-rc.0
+id-map@1.0.4-rc.0
 idmontie:migrations@1.0.0
-jquery@1.11.3_2
-json@1.0.3
+jquery@1.11.4-rc.0
 kadira:blaze-layout@2.0.0
 kadira:flow-router@2.3.0
 kenton:accounts-sandstorm@0.1.4
-launch-screen@1.0.2
-less@1.0.14
-livedata@1.0.13
-localstorage@1.0.3
-logging@1.0.7
+launch-screen@1.0.3-rc.0
+less@2.5.0-rc.1_1
+livedata@1.0.14-rc.0
+localstorage@1.0.4-rc.0
+logging@1.0.8-rc.0
 matb33:collection-hooks@0.7.14
-matteodem:easy-search@1.6.2
-meteor@1.1.6
-meteor-platform@1.2.2
+matteodem:easy-search@1.6.3
+meteor@1.1.7-rc.0
+meteor-base@1.0.1-rc.0
+meteor-platform@1.2.3-rc.0
 meteorhacks:aggregate@1.2.1
 meteorhacks:collection-utils@1.2.0
 meteorhacks:picker@1.0.3
 meteorhacks:subs-manager@1.5.2
 meteorspark:util@0.2.0
-minifiers@1.1.5
-minimongo@1.0.8
-mobile-status-bar@1.0.3
-mongo@1.1.0
-mongo-livedata@1.0.8
+minifiers@1.1.6-rc.1
+minimongo@1.0.9-rc.0
+mobile-status-bar@1.0.5-rc.0
+mongo@1.1.1-rc.1
+mongo-id@1.0.1-rc.0
+mongo-livedata@1.0.9-rc.0
 mousetrap:mousetrap@1.4.6_1
 mquandalle:autofocus@1.0.0
 mquandalle:bower@1.4.1_3
-mquandalle:jade@0.4.3
+mquandalle:jade@0.4.3_1
 mquandalle:jade-compiler@0.4.3
 mquandalle:jquery-textcomplete@0.3.9_1
 mquandalle:jquery-ui-drag-drop-sort@0.1.0
 mquandalle:moment@1.0.0
 mquandalle:stylus@1.1.1
 npm-bcrypt@0.7.8_2
-observe-sequence@1.0.6
+npm-mongo@1.4.39-rc.0_1
+observe-sequence@1.0.7-rc.0
 ongoworks:speakingurl@1.1.0
-ordered-dict@1.0.3
+ordered-dict@1.0.4-rc.0
 peerlibrary:assert@0.2.5
 peerlibrary:base-component@0.10.0
 peerlibrary:blaze-components@0.13.0
 perak:markdown@1.0.5
+promise@0.4.3-rc.0_1
 raix:eventemitter@0.1.3
 raix:handlebar-helpers@0.2.4
-random@1.0.3
-reactive-dict@1.1.0
-reactive-var@1.0.5
-reload@1.1.3
-retry@1.0.3
+random@1.0.4-rc.0
+rate-limit@1.0.0-rc.0
+reactive-dict@1.1.1-rc.0
+reactive-var@1.0.6-rc.0
+reload@1.1.4-rc.0
+retry@1.0.4-rc.0
 reywood:publish-composite@1.4.2
-routepolicy@1.0.5
+routepolicy@1.0.6-rc.0
 seriousm:emoji-continued@1.4.0
-service-configuration@1.0.4
-session@1.1.0
-sha@1.0.3
+service-configuration@1.0.5-rc.0
+session@1.1.1-rc.0
+sha@1.0.4-rc.0
 softwarerero:accounts-t9n@1.0.9
-spacebars@1.0.6
-spacebars-compiler@1.0.6
-srp@1.0.3
+spacebars@1.0.7-rc.0
+spacebars-compiler@1.0.7-rc.0
+srp@1.0.4-rc.0
+standard-minifiers@1.0.0-rc.1
 tap:i18n@1.5.1
 templates:tabs@2.2.0
-templating@1.1.1
+templating@1.1.2-rc.2
+templating-tools@1.0.0-rc.0
 tmeasday:presence@1.0.6
-tracker@1.0.7
-ui@1.0.6
-underscore@1.0.3
-url@1.0.4
-useraccounts:core@1.12.2
-useraccounts:flow-routing@1.12.2
-useraccounts:unstyled@1.12.2
-webapp@1.2.0
-webapp-hashing@1.0.3
+tracker@1.0.8-rc.0
+ui@1.0.7-rc.0
+underscore@1.0.4-rc.0
+url@1.0.5-rc.0
+useraccounts:core@1.12.3
+useraccounts:flow-routing@1.12.3
+useraccounts:unstyled@1.12.3
+webapp@1.2.2-rc.0
+webapp-hashing@1.0.4-rc.0
 zimme:active-route@2.3.1

client/components/forms/inlinedform.js
@@ -70,12 +70,10 @@ BlazeComponent.extendComponent({
 
       // Close the inlined form when after its submission
       submit: function() {
-        var self = this;
-        // XXX Swith to an arrow function here when we'll have ES6
         if (this.currentData().autoclose !== false) {
-          Tracker.afterFlush(function() {
-            self.close();
-            self.callFirstWith(self, 'resetCache');
+          Tracker.afterFlush(() => {
+            this.close();
+            this.callFirstWith(this, 'resetCache');
           });
         }
       }

commit 60712e1ac4c431f6c7d8c992a6c7e75ac82a3ab7

Fix EscapeActions click in handling
Fixes a bug introduced in 07cc454 and one introduced in 22e854c.

client/components/cards/details.js
@@ -118,6 +118,6 @@ Template.cardMorePopup.events({
 EscapeActions.register('detailsPane',
   function() { Utils.goBoardId(Session.get('currentBoard')); },
   function() { return ! Session.equals('currentCard', null); }, {
-    noClickEscapeOn: '.js-card-details'
+    noClickEscapeOn: '.js-card-details,.board-sidebar,#header'
   }
 );

client/config/router.js
@@ -14,9 +14,14 @@ FlowRouter.route('/', {
 FlowRouter.route('/b/:id/:slug', {
   name: 'board',
   action: function(params) {
-    EscapeActions.executeAll();
+    let currentBoard = params.id;
+    // If we close a card, we'll execute again this route action but we don't
+    // want to excape every current actions (filters, etc.)
+    if (Session.get('currentBoard') !== currentBoard) {
+      EscapeActions.executeAll();
+    }
 
-    Session.set('currentBoard', params.id);
+    Session.set('currentBoard', currentBoard);
     Session.set('currentCard', null);
 
     BlazeLayout.render('defaultLayout', { content: 'board' });

client/lib/escapeActions.js
@@ -18,19 +18,25 @@ EscapeActions = {
   ],
 
   register: function(label, action, condition = () => true, options = {}) {
-    var priority = this.hierarchy.indexOf(label);
+    const priority = this.hierarchy.indexOf(label);
     if (priority === -1) {
       throw Error('You must define the label in the EscapeActions hierarchy');
     }
 
-    this._actions.push({
+    let enabledOnClick = options.enabledOnClick;
+    if (_.isUndefined(enabledOnClick)) {
+      enabledOnClick = true;
+    }
+
+    let noClickEscapeOn = options.noClickEscapeOn;
+
+    this._actions[priority] = {
       priority,
       condition,
       action,
-      noClickEscapeOn: options.noClickEscapeOn,
-      enabledOnClick: !! options.enabledOnClick
-    });
-    this._actions = _.sortBy(this._actions, (a) => { return a.priority; });
+      noClickEscapeOn,
+      enabledOnClick
+    };
   },
 
   executeLowest: function() {
@@ -69,27 +75,28 @@ EscapeActions = {
   },
 
   _execute: function(options) {
-    var maxLabel = options.maxLabel;
-    var multipleActions = options.multipleActions;
-    var isClick = !! options.isClick;
-    var clickTarget = options.clickTarget;
+    const maxLabel = options.maxLabel;
+    const multipleActions = options.multipleActions;
+    const isClick = !! options.isClick;
+    const clickTarget = options.clickTarget;
 
-    var maxPriority, currentAction;
-    var executedAtLeastOne = false;
+    let executedAtLeastOne = false;
+    let maxPriority;
+
     if (! maxLabel)
       maxPriority = Infinity;
     else
       maxPriority = this.hierarchy.indexOf(maxLabel);
 
-    for (var i = 0; i < this._actions.length; i++) {
-      currentAction = this._actions[i];
+    for (let i = 0; i < this._actions.length; i++) {
+      let currentAction = this._actions[i];
       if (currentAction.priority > maxPriority)
         return executedAtLeastOne;
 
       if (isClick && this._stopClick(currentAction, clickTarget))
         return executedAtLeastOne;
 
-      var isEnabled = currentAction.enabledOnClick || ! isClick;
+      let isEnabled = currentAction.enabledOnClick || ! isClick;
       if (isEnabled && currentAction.condition()) {
         currentAction.action();
         executedAtLeastOne = true;
@@ -150,8 +157,8 @@ Mousetrap.bindGlobal('esc', function() {
 // On a left click on the document, we try to exectute one escape action (eg,
 // close the popup). We don't execute any action if the user has clicked on a
 // link or a button.
-$(document.body).on('click', function(evt) {
-  if (evt.which === 1 &&
+$(document).on('click', function(evt) {
+  if (evt.button === 0 &&
     $(evt.target).closest('a,button,.is-editable').length === 0) {
     EscapeActions.clickExecute(evt.target, 'multiselection');
   }