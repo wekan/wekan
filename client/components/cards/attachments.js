ed35a88a5689ed460d92a4990bc4882b7934854f
Maxime Quandalle <maxime@quandalle.com>
2015-09-02 20:16:53 +0200

    Fix attachments downloading
    Fixes #219
    Fixes #256

--- client/components/cards/attachments.js
+++ client/components/cards/attachments.js
@@ -6,10 +6,13 @@ Template.attachmentsGalery.events({
       Popup.close();
     }
   ),
-  // If we let this event bubble, Iron-Router will handle it and empty the
-  // page content, see #101.
-  'click .js-open-viewer, click .js-download': function(event) {
+  // If we let this event bubble, FlowRouter will handle it and empty the page
+  // content, see #101.
+  'click .js-download': function(event) {
     event.stopPropagation();
+  },
+  'click .js-open-viewer': function() {
+    // XXX Not implemented!
   },
   'click .js-add-cover': function() {
     Cards.update(this.cardId, { $set: { coverId: this._id } });
