7c473846e4005c252cbaae5ec500d703cfc6adce
Liming Xie <liming.xie@gmail.com>
2015-11-08 14:23:32 +0800

    add: import members/labels/lists as template from other board

--- client/components/boards/boardHeader.js
+++ client/components/boards/boardHeader.js
@@ -13,6 +13,7 @@ Template.boardMenuPopup.events({
     // confirm that the board was successfully archived.
     FlowRouter.go('home');
   }),
+  'click .js-clone-template': Popup.open('cloneBoardTemplate'),
 });
 
 Template.boardChangeTitlePopup.events({
@@ -174,3 +175,132 @@ BlazeComponent.extendComponent({
     }];
   },
 }).register('boardChangeVisibilityPopup');
+
+BlazeComponent.extendComponent({
+  template() {
+    return 'cloneBoardTemplatePopup';
+  },
+
+  boards() {
+    return Boards.find({
+      archived: false,
+      'members.userId': Meteor.userId(),
+    }, {
+      sort: ['title'],
+    });
+  },
+
+  currentBoardId() {
+    return Session.get('currentBoard');
+  },
+
+  copyLabels(to, labels) {
+    if(to.labels) {
+      // reuse if same
+      to.labels.forEach((label) => {
+        if(!_.findWhere(labels, {name:label.name, color:label.color})) {
+          to.removeLabel(label._id);
+        }
+      });
+    }
+    if (labels) {
+      labels.forEach((label) => {
+        to.addLabel(label.name, label.color);
+      });
+    }
+  },
+
+  copyMembers(to, members) {
+    if(to.members) {
+      // reuse if same
+      to.members.forEach((member) => {
+        if(member.userId !== Meteor.userId()) {
+          if(!_.findWhere(members, {userId:member.userId})) {
+            to.removeMember(member.userId);
+          }
+        }
+      });
+    }
+    if (members){
+      members.forEach((member) => {
+        if(member.userId !== Meteor.userId()) {
+          to.addMember(member.userId);
+          to.setMemberPermission(member.userId, member.isAdmin);
+        }
+      });
+    }
+  },
+
+  copyLists(to, lists) {
+    // skip archived lists from source
+    const otherLists = [];
+    lists.forEach((list) => {
+      if(!list.archived) otherLists.push(list);
+    });
+    let i = 0;
+    // we reuse and rename the existing lists, and archive the rest
+    const myLists = Lists.find({boardId: to._id}, { sort: ['sort'] }).fetch();
+    myLists.forEach((list) => {
+      if(list.archived) return;
+      if(i < otherLists.length) {
+        const other = otherLists[i++];
+        Lists.update(list._id, {
+          $set: {
+            title: other.title,
+            sort: other.sort,
+            status: other.status,
+          },
+        });
+      } else {
+        list.archive();
+      }
+    });
+    // if not enough, create new lists
+    while(i < otherLists.length) {
+      const other = otherLists[i++];
+      Lists.insert({
+        boardId: Session.get('currentBoard'),
+        title: other.title,
+        sort: other.sort,
+        status: other.status,
+      });
+    }
+  },
+
+  cloneTemplate(fromId, callback) {
+    const toId = Session.get('currentBoard');
+    new SubsManager().subscribe('board', toId);
+    new SubsManager().subscribe('board', fromId);
+    // after subscribe, the data may sync in a while, so we check after some delay
+    const self = this;
+    let loopMax = 60;
+    (function checkDataLoop() {
+      const lists = Lists.find({boardId: fromId}).fetch();
+      if (lists.length > 0) {
+        const to = Boards.findOne(toId);
+        const from = Boards.findOne(fromId);
+        self.copyLabels(to, from.labels);
+        self.copyMembers(to, from.members);
+        self.copyLists(to, lists);
+        callback();
+      } else if(--loopMax > 0) {
+        window.setTimeout(checkDataLoop, 50);
+      }
+    })();
+  },
+
+  events() {
+    return [{
+      'click .js-clone-from-board'(evt, tpl) {
+        const fromId = $(evt.currentTarget).attr('id').trim();
+        if(fromId) {
+          Popup.afterConfirm('confirmCloneTemplate', () => {
+            this.cloneTemplate(fromId, () => {
+              Popup.close();
+            });
+          }).call(this, evt, tpl);
+        }
+      },
+    }];
+  },
+}).register('cloneBoardTemplatePopup');
