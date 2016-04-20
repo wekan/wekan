wekan/wekan

commit aa974aa54ab6e5b7db7450206d12b44ffb3a0306

Prefer ES5 methods over underscore utilities
Since 07cc454 (ie the switch to Meteor 1.2) we includes the `es5-shim`
polyfill to support methods like `Array.prototype.forEach` in a
consistent way across all supported browsers (IE8+).

MDG recently released a blog post recommending the use of these native
methods instead of underscore [0]. We know follow this recommendation.

This commit also favor some ES6 features (argument defaults,
destructing assignment) in places where we didn’t use them.

[0]: http://info.meteor.com/blog/es2015-get-started

models/users.js
@@ -14,13 +14,13 @@ Users.helpers({
   },
 
   starredBoards() {
-    const starredBoardIds = this.profile.starredBoards || [];
-    return Boards.find({archived: false, _id: {$in: starredBoardIds}});
+    const {starredBoards = []} = this.profile;
+    return Boards.find({archived: false, _id: {$in: starredBoards}});
   },
 
   hasStarred(boardId) {
-    const starredBoardIds = this.profile.starredBoards || [];
-    return _.contains(starredBoardIds, boardId);
+    const {starredBoards = []} = this.profile;
+    return _.contains(starredBoards, boardId);
   },
 
   isBoardMember() {
@@ -54,9 +54,9 @@ Users.helpers({
       return profile.initials;
 
     else if (profile.fullname) {
-      return _.reduce(profile.fullname.split(/\s+/), (memo, word) => {
+      return profile.fullname.split(/\s+/).reduce((memo = '', word) => {
         return memo + word[0];
-      }, '').toUpperCase();
+      }).toUpperCase();
 
     } else {
       return this.username[0].toUpperCase();
@@ -130,7 +130,7 @@ if (Meteor.isServer) {
     // b. We use it to find deleted and newly inserted ids by using it in one
     // direction and then in the other.
     function incrementBoards(boardsIds, inc) {
-      _.forEach(boardsIds, (boardId) => {
+      boardsIds.forEach((boardId) => {
         Boards.update(boardId, {$inc: {stars: inc}});
       });
     }
@@ -149,7 +149,7 @@ if (Meteor.isServer) {
     // Insert the Welcome Board
     Boards.insert(ExampleBoard, (err, boardId) => {
 
-      _.forEach(['Basics', 'Advanced'], (title) => {
+      ['Basics', 'Advanced'].forEach((title) => {
         const list = {
           title,
           boardId,

client/components/cards/cardDetails.js
@@ -62,7 +62,8 @@ BlazeComponent.extendComponent({
       },
     };
 
-    return [_.extend(events, {
+    return [{
+      ...events,
       'click .js-close-card-details'() {
         Utils.goBoardId(this.data().boardId);
       },
@@ -86,7 +87,7 @@ BlazeComponent.extendComponent({
         this.parentComponent().showOverlay.set(true);
         this.parentComponent().mouseHasEnterCardDetails = true;
       },
-    })];
+    }];
   },
 }).register('cardDetails');
 

client/components/cards/labels.js
@@ -13,7 +13,7 @@ BlazeComponent.extendComponent({
   },
 
   labels() {
-    return _.map(labelColors, (color) => {
+    return labelColors.map((color) => {
       return { color, name: '' };
     });
   },

client/components/main/editor.js
@@ -54,7 +54,7 @@ const at = HTML.CharRef({html: '&commat;', str: '@'});
 Blaze.Template.registerHelper('mentions', new Template('mentions', function() {
   const view = this;
   const currentBoard = Boards.findOne(Session.get('currentBoard'));
-  const knowedUsers = _.map(currentBoard.members, (member) => {
+  const knowedUsers = currentBoard.members.map((member) => {
     member.username = Users.findOne(member.userId).username;
     return member;
   });

client/components/sidebar/sidebar.js
@@ -95,10 +95,10 @@ BlazeComponent.extendComponent({
   events() {
     // XXX Hacky, we need some kind of `super`
     const mixinEvents = this.getMixin(Mixins.InfiniteScrolling).events();
-    return mixinEvents.concat([{
+    return [...mixinEvents, {
       'click .js-toggle-sidebar': this.toggle,
       'click .js-back-home': this.setView,
-    }]);
+    }];
   },
 }).register('sidebar');
 

client/components/users/userHeader.js
@@ -41,7 +41,7 @@ Template.changePasswordPopup.onRendered(function() {
 
 Template.changeLanguagePopup.helpers({
   languages() {
-    return _.map(TAPi18n.getLanguages(), (lang, tag) => {
+    return TAPi18n.getLanguages().map((lang, tag) => {
       const name = lang.name;
       return { tag, name };
     });

client/config/accounts.js
@@ -25,7 +25,7 @@ AccountsTemplates.configure({
   },
 });
 
-_.each(['signIn', 'signUp', 'resetPwd', 'forgotPwd', 'enrollAccount'],
+['signIn', 'signUp', 'resetPwd', 'forgotPwd', 'enrollAccount'].forEach(
   (routeName) => AccountsTemplates.configureRoute(routeName));
 
 // We display the form to change the password in a popup window that already

client/lib/filter.js
@@ -95,7 +95,7 @@ Filter = {
       return {};
 
     const filterSelector = {};
-    _.forEach(this._fields, (fieldName) => {
+    this._fields.forEach((fieldName) => {
       const filter = this[fieldName];
       if (filter._isActive())
         filterSelector[fieldName] = filter._getMongoSelector();
@@ -116,7 +116,7 @@ Filter = {
   },
 
   reset() {
-    _.forEach(this._fields, (fieldName) => {
+    this._fields.forEach((fieldName) => {
       const filter = this[fieldName];
       filter.reset();
     });

commit c11ed6b3419784a628fe1993b26e5b227fb4e011

Fix initials avatar generation
The ES5 reduce method also needs a initial value. This bug was
introduced in aa974aa54ab6e5b7db7450206d12b44ffb3a0306.

models/users.js
@@ -68,9 +68,9 @@ Users.helpers({
       return profile.initials;
 
     else if (profile.fullname) {
-      return profile.fullname.split(/\s+/).reduce((memo = '', word) => {
+      return profile.fullname.split(/\s+/).reduce((memo, word) => {
         return memo + word[0];
-      }).toUpperCase();
+      }, '').toUpperCase();
 
     } else {
       return this.username[0].toUpperCase();