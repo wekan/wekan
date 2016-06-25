4d7560069b57d4cb95816d6e10af1d98cf003f15
Kuikui <kuikui.liu@quantumhealth.cn>
2016-06-25 18:31:07 +0800

    remove useless shell files

--- run-email.sh
+++
@@ -1,1 +0,0 @@
-MONGO_URL=mongodb://localhost:27017/wekan MAIL_URL=smtp://noreplytest:noreply110@smtp.126.com:25/ MAIL_FROM=noreplytest@126.com meteor
--- run.sh
+++
@@ -1,1 +0,0 @@
-MONGO_URL=mongodb://localhost:27017/wekan meteor
