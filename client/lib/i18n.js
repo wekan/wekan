1c206d0d367cbb4b6aa2e350ce944397bd48b988
David Renshaw <david@sandstorm.io>
2016-11-10 10:10:06 -0500

    Fix startup for clients using Finnish and Chinese.

--- client/lib/i18n.js
+++ client/lib/i18n.js
@@ -14,8 +14,14 @@ Tracker.autorun(() => {
   if (language) {
     TAPi18n.setLanguage(language);
 
-    // XXX
-    const shortLanguage = language.split('-')[0];
-    T9n.setLanguage(shortLanguage);
+    // For languages such as Finnish (Suomi) that are not supported by meteor-accounts-t9n,
+    // the following may throw an exception. On the initial run of this `autorun()` callback,
+    // such an exception could cause the entire app to fail to load. Therefore, we catch
+    // the exception and log it as an error.
+    try {
+      T9n.setLanguage(language);
+    } catch (e) {
+      console.error(e);
+    }
   }
 });
