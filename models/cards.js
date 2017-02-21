2514f512c5586bb58309e3f42279386ae03d1acd
Florent MONTHEL <fmonthel@flox-arts.net>
2017-02-20 23:55:50 -0500

    [BUG] Fix email adn add comment in code

--- models/cards.js
+++ models/cards.js
@@ -340,6 +340,7 @@ if (Meteor.isServer) {
     // Say goodbye to the former member
     if (modifier.$pull && modifier.$pull.members) {
       memberId = modifier.$pull.members;
+	  // Check that the former member is member of the card
       if (_.contains(doc.members, memberId)) {
         Activities.insert({
           userId,
