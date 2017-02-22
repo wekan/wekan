472c23f9feb3bf5cb31f89feda4c8b182e87b3e3
Alexis LACROIX <alexis.lacroix@orange.com>
2017-02-22 15:03:33 +0100

    Improve wekan performance (checklist collection scan)

--- models/checklists.js
+++ models/checklists.js
@@ -133,6 +133,10 @@ Checklists.mutations({
 });
 
 if (Meteor.isServer) {
+  Meteor.startup(() => {
+    Checklists._collection._ensureIndex({ cardId: 1, createdAt: 1 });
+  });
+
   Checklists.after.insert((userId, doc) => {
     Activities.insert({
       userId,
