5dd79d78fd0083d2c3ead4c3fa6c22506114e429
Joel Louzado <jlouzado@gmail.com>
2017-03-13 16:49:56 +0530

    Created board-specific progress Bar

--- client/components/progressbar/progressbar.js
+++ client/components/progressbar/progressbar.js
@@ -1,18 +1,56 @@
-(class ProgressBar extends BlazeComponent {
+const ProgressSubs = new SubsManager();
+
+class ProgressBar extends BlazeComponent {
   template() {
     return 'progressBarTemplate';
   }
 
   onCreated() {
     super.onCreated();
-    this.value = new ReactiveVar(this.data().prog);
+    const self = this;
+    self.state = new ReactiveDict('progressBarState');
+    self.state.set('ready', false);
   }
 
   progressValue() {
-    return this.value.get();
+    const completed = this.completedItems();
+    const total = this.totalItems();
+    if(total > 0)
+      return (completed / total) * 100;
+    else
+      return;
   }
 
   progressCSS() {
-    return "width:"+ this.value.get() +"%";
+    return "width:"+ this.progressValue() +"%";
   }
-}).register("progressBar");
+
+  ready() {
+    return this.state.get('ready');
+  }
+}
+
+(class BoardProgressBar extends ProgressBar {
+  onCreated() {
+    super.onCreated();
+    this.autorun(() => {
+      const currentBoardId = Session.get('currentBoard');
+      if (!currentBoardId)
+        return;
+      const listsHandle = ProgressSubs.subscribe('boardLists', currentBoardId);
+      const cardsHandle = ProgressSubs.subscribe('boardCards', currentBoardId);
+
+      const subsReady = (listsHandle.ready() && cardsHandle.ready());
+      this.state.set('ready', subsReady);
+    });
+  }
+
+  totalItems() {
+    return Cards.find({}).count();
+  }
+
+  completedItems() {
+    const completedList = Lists.findOne({}, {sort: {sort: -1}});
+    return Cards.find({listId: completedList["_id"]}).count();
+  }
+}).register('boardProgressBar');
