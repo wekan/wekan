383daf6f6416e6e3dfe98cf9fcc5528948f35999
Ryan Helsing <ryanhelsing@gmail.com>
2017-03-18 15:22:40 -0400

    cant see add list if comment only

--- models/users.js
+++ models/users.js
@@ -117,6 +117,11 @@ if (Meteor.isClient) {
       return board && board.hasMember(this._id);
     },
 
+    isCommentOnly() {
+      const board = Boards.findOne(Session.get('currentBoard'));
+      return board && board.hasCommentOnly(this._id);
+    },
+
     isBoardAdmin() {
       const board = Boards.findOne(Session.get('currentBoard'));
       return board && board.hasAdmin(this._id);
