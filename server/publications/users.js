a314d608a2dd4e05caeb50d76f18ba84f3981c8c
Stephen Moloney <stephen@stephenmoloney.com>
2017-04-06 09:25:57 +0100

    remove tailing commas

--- server/publications/users.js
+++ server/publications/users.js
@@ -5,15 +5,15 @@ Meteor.publish('user-miniprofile', function(userId) {
     fields: {
       'username': 1,
       'profile.fullname': 1,
-      'profile.avatarUrl': 1,
-    },
+      'profile.avatarUrl': 1
+    }
   });
 });
 
 Meteor.publish('user-admin', function() {
   return Meteor.users.find(this.userId, {
     fields: {
-      isAdmin: 1,
-    },
+      isAdmin: 1
+    }
   });
 });
