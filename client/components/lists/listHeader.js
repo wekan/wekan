bee9bf2226d731f513ed2146cee28ab844c06a63
Lauri Ojansivu <x@xet7.org>
2017-06-02 19:18:31 +0300

    Fix lint errors.

--- client/components/lists/listHeader.js
+++ client/components/lists/listHeader.js
@@ -1,73 +1,74 @@
 BlazeComponent.extendComponent({
   editTitle(evt) {
-      evt.preventDefault();
-      const newTitle = this.childComponents('inlinedForm')[0].getValue().trim();
-      const list = this.currentData();
-      if (newTitle) {
-          list.rename(newTitle.trim());
-      }
+    evt.preventDefault();
+    const newTitle = this.childComponents('inlinedForm')[0].getValue().trim();
+    const list = this.currentData();
+    if (newTitle) {
+      list.rename(newTitle.trim());
+    }
   },
 
   isWatching() {
-      const list = this.currentData();
-      return list.findWatcher(Meteor.userId());
+    const list = this.currentData();
+    return list.findWatcher(Meteor.userId());
   },
 
   limitToShowCardsCount() {
-      return Meteor.user().getLimitToShowCardsCount();
+    return Meteor.user().getLimitToShowCardsCount();
   },
 
   showCardsCountForList(count) {
-      return count > this.limitToShowCardsCount();
+    return count > this.limitToShowCardsCount();
   },
 
   events() {
-      return [{
-          'click .js-open-list-menu': Popup.open('listAction'),
-          submit: this.editTitle,
-      }];
+    return [{
+      'click .js-open-list-menu': Popup.open('listAction'),
+      submit: this.editTitle,
+    }];
   },
 }).register('listHeader');
 
 Template.listActionPopup.helpers({
   isWatching() {
-      return this.findWatcher(Meteor.userId());
+    return this.findWatcher(Meteor.userId());
   },
 });
 
 Template.listActionPopup.events({
   'click .js-add-card' () {
-      const listDom = document.getElementById(`js-list-${this._id}`);
-      const listComponent = BlazeComponent.getComponentForElement(listDom);
-      listComponent.openForm({ position: 'top' });
-      Popup.close();
+    const listDom = document.getElementById(`js-list-${this._id}`);
+    const listComponent = BlazeComponent.getComponentForElement(listDom);
+    listComponent.openForm({
+      position: 'top'
+    });
+    Popup.close();
   },
   'click .js-list-subscribe' () {},
   'click .js-select-cards' () {
-      const cardIds = this.allCards().map((card) => card._id);
-      MultiSelection.add(cardIds);
-      Popup.close();
+    const cardIds = this.allCards().map((card) => card._id);
+    MultiSelection.add(cardIds);
+    Popup.close();
   },
   'click .js-toggle-watch-list' () {
-      const currentList = this;
-      const level = currentList.findWatcher(Meteor.userId()) ? null : 'watching';
-      Meteor.call('watch', 'list', currentList._id, level, (err, ret) => {
-          if (!err && ret) Popup.close();
-      });
+    const currentList = this;
+    const level = currentList.findWatcher(Meteor.userId()) ? null : 'watching';
+    Meteor.call('watch', 'list', currentList._id, level, (err, ret) => {
+      if (!err && ret) Popup.close();
+    });
   },
   'click .js-close-list' (evt) {
-      evt.preventDefault();
-      this.archive();
-      Popup.close();
+    evt.preventDefault();
+    this.archive();
+    Popup.close();
   },
   'click .js-more': Popup.open('listMore'),
 });
 
 Template.listMorePopup.events({
-  'click .js-delete': Popup.afterConfirm('listDelete', function() {
-      Popup.close();
-      Lists.remove(this._id);
-      Utils.goBoardId(this.boardId);
+  'click .js-delete': Popup.afterConfirm('listDelete', function () {
+    Popup.close();
+    Lists.remove(this._id);
+    Utils.goBoardId(this.boardId);
   }),
 });
-
