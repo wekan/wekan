f0597ef0c496352cb102d85227a46831a8b04c57
Andrés Manelli <andresmanelli@gmail.com>
2018-04-20 23:52:13 -0300

    Indicate board or card through icons. Indicate if archived

--- client/components/cards/minicard.js
+++ client/components/cards/minicard.js
@@ -6,4 +6,15 @@ BlazeComponent.extendComponent({
   template() {
     return 'minicard';
   },
+
+  events() {
+    return [{
+      'click .js-imported-link' (evt) {
+        if (this.data().isImportedCard())
+          Utils.goCardId(this.data().importedId);
+        else if (this.data().isImportedBoard())
+          Utils.goBoardId(this.data().importedId);
+      },
+    }];
+  },
 }).register('minicard');
