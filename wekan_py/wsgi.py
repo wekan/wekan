e5c6da5a3919158d8164ae1e0b4a8567da71249d
SpeaKeasY <speakeasysky@gmail.com>
2018-05-20 18:38:33 +0000

    Added initial DJango 2.0.5 project config with pyjade, python 3.6.5 venv.

---
+++ wekan_py/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wekan_py project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wekan_py.settings")
+
+application = get_wsgi_application()
