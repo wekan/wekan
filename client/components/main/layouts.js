fa13e231a95e6a57a3a70c519449ccb0e767390a
guillaume <guillaume.cassou@supinfo.com>
2018-10-02 11:12:10 +0200

    LDAP support

--- client/components/main/layouts.js
+++ client/components/main/layouts.js
@@ -47,6 +47,52 @@ Template.userFormsLayout.events({
     T9n.setLanguage(i18nTagToT9n(i18nTag));
     evt.preventDefault();
   },
+  'click button#cas'() {
+    Meteor.loginWithCas(function() {
+      if (FlowRouter.getRouteName() === 'atSignIn') {
+        FlowRouter.go('/');
+      }
+    });
+  },
+  'submit form'(event) {
+    const connectionMethod = $('.select-connection').val();
+
+    // Local account
+    if (connectionMethod === 'default') {
+      return;
+    }
+
+    // TODO : find a way to block "submit #at-pwd-form" of the at_pwd_form.js
+
+    const inputs = event.target.getElementsByTagName('input');
+
+    const email = inputs.namedItem('at-field-username_and_email').value;
+    const password = inputs.namedItem('at-field-password').value;
+
+    // Ldap account
+    if (connectionMethod === 'ldap') {
+      // Check if the user can use the ldap connection
+      Meteor.subscribe('user-connection-method', email, {
+        onReady() {
+          const ldap = Users.findOne();
+          
+          if (ldap) {
+            // Use the ldap connection package
+            Meteor.loginWithLDAP(email, password, function(error) {
+              if (!error) {
+                // Connection
+                return FlowRouter.go('/');
+              }
+              
+              // TODO : add a gestion of the different cases of exceptions
+              console.log(error);
+            });
+          }
+          return this.stop();
+        }
+      });
+    }
+  },
 });
 
 Template.defaultLayout.events({
