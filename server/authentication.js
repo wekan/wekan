1178a48e68fcb99b538e5af33af69fe30b57e0ac
Lauri Ojansivu <x@xet7.org>
2019-06-13 11:03:45 +0300

    Add back [OAuth2 idTokenWhitelistFields default empty array](https://github.com/wekan/wekan/commit/8b31c0768c34fc4557b54cec936a0b4288a8e722#commitcomment-33921882).
    Thanks to moserben16 !

--- server/authentication.js
+++ server/authentication.js
@@ -76,7 +76,7 @@ Meteor.startup(() => {
             authorizationEndpoint: process.env.OAUTH2_AUTH_ENDPOINT,
             userinfoEndpoint: process.env.OAUTH2_USERINFO_ENDPOINT,
             tokenEndpoint: process.env.OAUTH2_TOKEN_ENDPOINT,
-            idTokenWhitelistFields: process.env.OAUTH2_ID_TOKEN_WHITELIST_FIELDS,
+            idTokenWhitelistFields: process.env.OAUTH2_ID_TOKEN_WHITELIST_FIELDS || [],
             requestPermissions: process.env.OAUTH2_REQUEST_PERMISSIONS,
           },
           // OAUTH2_ID_TOKEN_WHITELIST_FIELDS || [],
