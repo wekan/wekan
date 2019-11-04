7a5401d5f09fc888c02defb189a6a70f9b2725ab
Benjamin Andresen <bandresen@gmail.com>
2019-11-04 08:10:11 +0100

    cardDate: endDate coloring change
    if no due-date timestamp is set => Gray
    if end-date timestamp is younger than due-date timestamp => Green
    if end-date timestamp is older than due-date timestamp => Red
    
    resolves #2741

--- client/components/cards/cardDate.js
+++ client/components/cards/cardDate.js
@@ -344,9 +344,9 @@ class CardEndDate extends CardDate {
     let classes = 'end-date' + ' ';
     const dueAt = this.data().getDue();
     const theDate = this.date.get();
-    if (theDate.diff(dueAt, 'days') >= 2) classes += 'long-overdue';
-    else if (theDate.diff(dueAt, 'days') >= 0) classes += 'due';
-    else if (theDate.diff(dueAt, 'days') >= -2) classes += 'almost-due';
+    if (!dueAt) classes += ''
+    else if (theDate.isBefore(dueAt)) classes += 'current'
+    else if (theDate.isAfter(dueAt)) classes += 'due'
     return classes;
   }
 
