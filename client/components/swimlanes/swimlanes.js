c93ea33d05bed042a744fc11e4730c9e5ecdb93b
Lauri Ojansivu <x@xet7.org>
2019-11-17 01:36:33 +0200

    Fix prettify.

--- client/components/swimlanes/swimlanes.js
+++ client/components/swimlanes/swimlanes.js
@@ -136,16 +136,16 @@ BlazeComponent.extendComponent({
     initSortable(boardComponent, $listsDom);
 
     // Minimize swimlanes start https://www.w3schools.com/howto/howto_js_accordion.asp
-    var acc = document.getElementsByClassName("accordion");
+    var acc = document.getElementsByClassName('accordion');
     var i;
     for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+      acc[i].addEventListener('click', function() {
+        this.classList.toggle('active');
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
         } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
+          panel.style.maxHeight = panel.scrollHeight + 'px';
         }
       });
     }
