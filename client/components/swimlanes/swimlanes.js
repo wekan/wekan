26e0bbce172f89baa380ddae19b7b495519db40f
Lauri Ojansivu <x@xet7.org>
2019-11-17 01:20:26 +0200

    Swimlanes collapsed by default.
    TODO:
    - Add count.
    - Move list names to top, if possible. I did not get it working yet.
    - Try to fit collapse+swimlane name etc at same row.
    
    Related #2804

--- client/components/swimlanes/swimlanes.js
+++ client/components/swimlanes/swimlanes.js
@@ -134,6 +134,22 @@ BlazeComponent.extendComponent({
     }
 
     initSortable(boardComponent, $listsDom);
+
+    // Minimize swimlanes start https://www.w3schools.com/howto/howto_js_accordion.asp
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+    // Minimize swimlanes end https://www.w3schools.com/howto/howto_js_accordion.asp
   },
   onCreated() {
     this.draggingActive = new ReactiveVar(false);
