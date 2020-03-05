aaecac091209e90c0c2123830728f5e7a835ccb4
Lauri Ojansivu <x@xet7.org>
2020-03-05 22:02:52 +0200

    Mouse scroll part 2.

--- server/scroll.js
+++ server/scroll.js
@@ -1,20 +1,14 @@
 Meteor.startup(() => {
   // Mouse Scroll Intertia, issue #2949. Integer.
   if (process.env.SCROLLINERTIA !== '0') {
-    Meteor.settings.public.SCROLLINERTIA = parseInt(
-      process.env.SCROLLINERTIA,
-      radix,
-    );
+    Meteor.settings.public.SCROLLINERTIA = process.env.SCROLLINERTIA;
   } else {
     Meteor.settings.public.SCROLLINERTIA = 0;
   }
 
   // Mouse Scroll Amount, issue #2949. "auto" or Integer.
   if (process.env.SCROLLAMOUNT !== 'auto') {
-    Meteor.settings.public.SCROLLAMOUNT = parseInt(
-      process.env.SCROLLAMOUNT,
-      radix,
-    );
+    Meteor.settings.public.SCROLLAMOUNT = process.env.SCROLLAMOUNT;
   } else {
     Meteor.settings.public.SCROLLAMOUNT = 'auto';
   }
