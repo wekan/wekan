393482315f8adf8e9752cc70f427f0c703f9c2ea
Lauri Ojansivu <x@xet7.org>
2020-03-20 14:02:29 +0200

    Fix typo.

--- server/richer-editor-setting-helper.js
+++ server/richer-editor-setting-helper.js
@@ -1,10 +1,10 @@
 Meteor.startup(() => {
-
   // Disable comment WYSIWYG editor for everyone to fix
   // Pasting text into a card is adding a line before and after
   // (and multiplies by pasting more)
   // https://github.com/wekan/wekan/issues/2890
-  Meteor.settings.public.RICHER_CARD_COMMENT_EDITOR = 'false';
+  const RCCE = 'false';
+  Meteor.settings.public.RICHER_CARD_COMMENT_EDITOR = RCCE;
 
   //// Old original code for setting:
   //const RCCE = process.env.RICHER_CARD_COMMENT_EDITOR;
