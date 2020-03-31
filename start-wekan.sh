c60a092fc0ed9fe15c417bcb443b1e3e3aaedf7e
Lauri Ojansivu <x@xet7.org>
2020-04-01 00:34:21 +0300

    Fix start-wekan.sh MongoDB port to 27017.
    Thanks to Keelan and xet7 !
    
    Related #2979

--- start-wekan.sh
+++ start-wekan.sh
@@ -7,7 +7,7 @@
       # Debug OIDC OAuth2 etc.
       #export DEBUG=true
       #---------------------------------------------
-      export MONGO_URL='mongodb://127.0.0.1:27018/wekan'
+      export MONGO_URL='mongodb://127.0.0.1:27017/wekan'
       #---------------------------------------------
       # Production: https://example.com/wekan
       # Local: http://localhost:2000
