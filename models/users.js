wekan/wekan

commit 9ae20a3f51e63c29f536e2f5b3e66a2c7d88c691

Fix Cards and Users docs not generated because of syntax error and new Javascript syntax. Wekan uses wekan/releases/generate-docs*.sh Python code to generate OpenAPI docs, it did not show any errors while generating docs, only left out parts of API docs.
Thanks to pvcon13 and xet7 !

models/users.js
@@ -531,7 +531,7 @@ Users.helpers({
   getStartDayOfWeek() {
     const profile = this.profile || {};
     // default is 'Monday' (1)
-    return profile.startDayOfWeek ?? 1;
+    return profile.startDayOfWeek || 1;
   },
 
   getTemplatesBoardId() {

models/cards.js
@@ -1053,6 +1053,7 @@ Cards.helpers({
       return Users.find({ _id: { $in: this.vote.positive } });
     return [];
   },
+
   voteMemberNegative() {
     if (this.vote && this.vote.negative)
       return Users.find({ _id: { $in: this.vote.negative } });
@@ -1475,12 +1476,12 @@ Cards.mutations({
       },
     };
   },
-  setVoteQuestion(question, public) {
+  setVoteQuestion(question, publicVote) {
     return {
       $set: {
         vote: {
           question,
-          public,
+          public: publicVote,
           positive: [],
           negative: [],
         },

commit 153d729544fb5bbfa5fa40d236303cbc01352334

Fix getStartDayOfWeek function
In case profile.startDayOfWeek is 0 it's evaluated to false and 1 is returned.
Let's fix this by differentiating between undefined and an actual value.

Fixes: 9ae20a3f51e63c29f536e2f5b3e66a2c7d88c691

models/users.js
@@ -530,8 +530,11 @@ Users.helpers({
 
   getStartDayOfWeek() {
     const profile = this.profile || {};
-    // default is 'Monday' (1)
-    return profile.startDayOfWeek || 1;
+    if (typeof profile.startDayOfWeek === 'undefined') {
+      // default is 'Monday' (1)
+      return 1;
+    }
+    return profile.startDayOfWeek;
   },
 
   getTemplatesBoardId() {