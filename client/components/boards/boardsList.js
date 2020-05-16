3718dd4d45f4ec364165f63402d4758f424c5bd1
Lauri Ojansivu <x@xet7.org>
2020-05-16 03:15:14 +0300

    Fix lint.

--- client/components/boards/boardsList.js
+++ client/components/boards/boardsList.js
@@ -72,7 +72,7 @@ BlazeComponent.extendComponent({
 
     // Disable drag-dropping if the current user is not a board member or is comment only
     this.autorun(() => {
-     if (Utils.isMiniScreen()) {
+      if (Utils.isMiniScreen()) {
         $boards.sortable({
           handle: '.board-handle',
         });
