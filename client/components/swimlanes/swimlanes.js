61e682470cdaef42cce2d74b41fb752cfc61848b
Lauri Ojansivu <x@xet7.org>
2020-06-08 17:33:38 +0300

    Default view Swimlanes part 2.
    Thanks to xet7 !

--- client/components/swimlanes/swimlanes.js
+++ client/components/swimlanes/swimlanes.js
@@ -26,11 +26,12 @@ function currentCardIsInThisList(listId, swimlaneId) {
   // OLD: Default view: board-view-lists
   ////else return currentCard && currentCard.listId === listId;
   // NEW: Default view: board-view-swimlanes
-else return (
-  currentCard &&
-  currentCard.listId === listId &&
-  currentCard.swimlaneId === swimlaneId
-);
+  else
+    return (
+      currentCard &&
+      currentCard.listId === listId &&
+      currentCard.swimlaneId === swimlaneId
+    );
 
   // https://github.com/wekan/wekan/issues/1623
   // https://github.com/ChronikEwok/wekan/commit/cad9b20451bb6149bfb527a99b5001873b06c3de
