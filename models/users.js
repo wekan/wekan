5755ece33e9fc5967c0b726abed0912fde9612db
Marc Hartmayer <hello@hartmayer.com>
2020-06-09 23:32:00 +0200

    Add user option to hide finished checklist items
    Add a user option to hide finished items in a checklist.

--- models/users.js
+++ models/users.js
@@ -128,6 +128,13 @@ Users.attachSchema(
       type: Boolean,
       optional: true,
     },
+    'profile.hideCheckedItems': {
+      /**
+       * does the user want to hide checked checklist items?
+       */
+      type: Boolean,
+      optional: true,
+    },
     'profile.hiddenSystemMessages': {
       /**
        * does the user want to hide system messages?
@@ -483,6 +490,11 @@ Users.helpers({
     return profile.showDesktopDragHandles || false;
   },
 
+  hasHideCheckedItems() {
+    const profile = this.profile || {};
+    return profile.hideCheckedItems || false;
+  },
+
   hasHiddenSystemMessages() {
     const profile = this.profile || {};
     return profile.hiddenSystemMessages || false;
@@ -612,6 +624,15 @@ Users.mutations({
     };
   },
 
+  toggleHideCheckedItems() {
+    const value = this.hasHideCheckedItems();
+    return {
+      $set: {
+        'profile.hideCheckedItems': !value,
+      },
+    };
+  },
+
   toggleSystem(value = false) {
     return {
       $set: {
@@ -689,6 +710,10 @@ Meteor.methods({
   toggleDesktopDragHandles() {
     const user = Meteor.user();
     user.toggleDesktopHandles(user.hasShowDesktopDragHandles());
+  },
+  toggleHideCheckedItems() {
+    const user = Meteor.user();
+    user.toggleHideCheckedItems();
   },
   toggleSystemMessages() {
     const user = Meteor.user();
