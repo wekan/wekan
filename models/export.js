b00db983c8506e0cdc9968e452c3c8025fc10776
Lauri Ojansivu <x@xet7.org>
2020-06-17 21:37:01 +0300

    Try to prevent errors on CSV/TSV export.
    Thanks to xet7 !
    
    Related #3173

--- models/export.js
+++ models/export.js
@@ -80,19 +80,19 @@ if (Meteor.isServer) {
       });
     }
     const exporter = new Exporter(boardId);
-    if (exporter.canExport(user)) {
-      body = params.query.delimiter
-        ? exporter.buildCsv(params.query.delimiter)
-        : exporter.buildCsv();
-      res.writeHead(200, {
-        'Content-Length': body.length,
-        'Content-Type': params.query.delimiter ? 'text/csv' : 'text/tsv',
-      });
-      res.write(body);
-      res.end();
-    } else {
-      res.writeHead(403);
-      res.end('Permission Error');
-    }
+    //if (exporter.canExport(user)) {
+    body = params.query.delimiter
+      ? exporter.buildCsv(params.query.delimiter)
+      : exporter.buildCsv();
+    //'Content-Length': body.length,
+    res.writeHead(200, {
+      'Content-Type': params.query.delimiter ? 'text/csv' : 'text/tsv',
+    });
+    res.write(body);
+    res.end();
+    //} else {
+    //  res.writeHead(403);
+    //  res.end('Permission Error');
+    //}
   });
 }
