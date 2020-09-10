f81fd8084fd6cd1ad57daefcc22ed1fb0acaaeca
Lauri Ojansivu <x@xet7.org>
2020-09-11 00:28:33 +0300

    Try to fix language names.
    Thanks to buplet, xoas and xet7.
    
    Fixes #3265

--- client/components/main/layouts.js
+++ client/components/main/layouts.js
@@ -74,10 +74,34 @@ Template.userFormsLayout.helpers({
       let name = lang.name;
       if (lang.name === 'br') {
         name = 'Brezhoneg';
+      } else if (lang.name === 'Español') {
+        name = 'español';
+      } else if (lang.name === 'Español de Argentina') {
+        name = 'español de Argentina';
+      } else if (lang.name === 'Español de Chile') {
+        name = 'español de Chile';
+      } else if (lang.name === 'Español de Colombia') {
+        name = 'español de Colombia';
+      } else if (lang.name === 'Español de México') {
+        name = 'español de México';
+      } else if (lang.name === 'Español de Puerto Rico') {
+        name = 'español de Puerto Rico';
+      } else if (lang.name === 'fr-BE') {
+        name = 'Français (Belgique)';
+      } else if (lang.name === 'fr-CA') {
+        name = 'Français (Canada)';
+      } else if (lang.name === 'fr-CA') {
+        name = 'Français (Canada)';
       } else if (lang.name === 'ig') {
         name = 'Igbo';
+      } else if (lang.name === 'lv') {
+        name = 'Latviešu';
+      } else if (lang.name === 'latviešu valoda') {
+        name = 'Latviešu';
       } else if (lang.name === 'oc') {
         name = 'Occitan';
+      } else if (lang.name === 'st') {
+        name = 'Sãotomense';
       } else if (lang.name === '繁体中文（台湾）') {
         name = '繁體中文（台灣）';
       }
