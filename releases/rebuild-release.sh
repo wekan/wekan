ee2ffc616a083dc1729a0d0cc4e2904bbb6a2bb9
David Arnold <dar@xoe.solutions>
2020-09-13 15:53:59 -0500

    rm fix-download-unicode

--- releases/rebuild-release.sh
+++ releases/rebuild-release.sh
@@ -10,7 +10,6 @@ rm -rf node_modules
 meteor npm install
 rm -rf .build
 METEOR_PROFILE=100 meteor build .build --directory
-cp -f fix-download-unicode/cfs_access-point.txt .build/bundle/programs/server/packages/cfs_access-point.js
 # Remove legacy webbroser bundle, so that Wekan works also at Android Firefox, iOS Safari, etc.
 rm -rf .build/bundle/programs/web.browser.legacy
 cd .build/bundle/programs/server
