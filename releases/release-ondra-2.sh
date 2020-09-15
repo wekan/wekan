e79b7fad0a35f29020c48a4a4eedb435573c9bf1
Lauri Ojansivu <x@xet7.org>
2020-09-15 23:14:05 +0300

    Update release scripts.

---
+++ releases/release-ondra-2.sh
@@ -0,0 +1,1 @@
+git push --follow-tags
--- releases/release.sh
+++ releases/release.sh
@@ -1,10 +1,22 @@
-# Usage: ./release.sh 1.36
+# Usage: ./release.sh 4.37
+
+# Commit and push version number changes
+cd ~/repos/wekan
+git add --all
+git commit -m "v$1"
+git push
+
+# Add release tag
+~/repos/wekan/releases/add-tag.sh v$1
+
+# Push to repo
+git push
 
 # Build Bundle
 ~/repos/wekan/releases/release-bundle.sh $1
 
 # Build Sandstorm
-~/repos/wekan/releases/release-sandstorm.sh $1
+#~/repos/wekan/releases/release-sandstorm.sh $1
 
 # Build Snap
 #./release-snap.sh $1
