90f73f6b700f7291884c2bcd0b7e2ba29cdb76cf
David Arnold <dar@xoe.solutions>
2020-09-16 09:50:09 -0500

    fixup! Mig: add migration cfs:* -> ostrio:files

--- server/migrations.js
+++ server/migrations.js
@@ -2,6 +2,7 @@ import AccountSettings from '../models/accountSettings';
 import Actions from '../models/actions';
 import Activities from '../models/activities';
 import Announcements from '../models/announcements';
+import Attachments from '../models/attachments';
 import Boards from '../models/boards';
 import CardComments from '../models/cardComments';
 import Cards from '../models/cards';
