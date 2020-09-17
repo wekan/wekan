2c175f2be604bb8650f5fa6d3aa73e8446642611
David Arnold <dar@xoe.solutions>
2020-09-16 22:57:06 -0500

    fixup! fix: cover image - works now

--- models/cards.js
+++ models/cards.js
@@ -750,7 +750,10 @@ Cards.helpers({
 
   cover() {
     if (!this.coverId) return false;
-    return Attachments.findOne(this.coverId);
+    const cover = Attachments.findOne(this.coverId);
+    // if we return a cover before it is fully stored, we will get errors when we try to display it
+    // todo XXX we could return a default "upload pending" image in the meantime?
+    return cover && cover.path && cover;
   },
 
   checklists() {
