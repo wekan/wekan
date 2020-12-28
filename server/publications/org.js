9e2093d6aed38e66fc4d63823315c9382e013a32
Lauri Ojansivu <x@xet7.org>
2020-12-28 21:08:27 +0200

    Teams/Organizations to Admin Panel. In Progress.
    Thanks to xet7 !
    
    Related #802

---
+++ server/publications/org.js
@@ -0,0 +1,27 @@
+Meteor.publish('org', function(query, limit) {
+  check(query, Match.OneOf(Object, null));
+  check(limit, Number);
+
+  if (!Match.test(this.userId, String)) {
+    return [];
+  }
+
+  const user = Users.findOne(this.userId);
+  if (user && user.isAdmin) {
+    return Org.find(query, {
+      limit,
+      sort: { createdAt: -1 },
+      fields: {
+        displayName: 1,
+        desc: 1,
+        name: 1,
+        website: 1,
+        teams: 1,
+        createdAt: 1,
+        loginDisabled: 1,
+      },
+    });
+  }
+
+  return [];
+});
