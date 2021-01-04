d171f4088f40512d321969df3f0c280a620c0c5f
Lauri Ojansivu <x@xet7.org>
2021-01-04 20:05:23 +0200

    Added many new translations to Wekan, now there is total 60 translations in Wekan. Updated translations. Organized pull-translations.sh alphabetically by language name.
    Thanks to translators and xet7 !

--- releases/translations/pull-translations.sh
+++ releases/translations/pull-translations.sh
@@ -3,6 +3,12 @@ cd ~/repos/wekan
 echo "Arabic:"
 tx pull -f -l ar
 
+echo "Armenian:"
+tx pull -f -l hy
+
+echo "Basque:"
+tx pull -f -l eu
+
 echo "Bulgarian:"
 tx pull -f -l bg_BG
 
@@ -12,50 +18,32 @@ tx pull -f -l br
 echo "Catalan:"
 tx pull -f -l ca
 
+echo "Chinese (China):"
+tx pull -f -l zh_CN
+
+echo "Chinese (Hong Kong):"
+tx pull -f -l zh_HK
+
+echo "Chinese (Taiwan):"
+tx pull -f -l zh_TW
+
+echo "Croatian:"
+tx pull -f -l hr
+
 echo "Czech:"
 tx pull -f -l cs
 
 echo "Danish:"
 tx pull -f -l da
 
-echo "Georgian:"
-tx pull -f -l ka
-
-echo "German:"
-tx pull -f -l de
-
-echo "Hindi:"
-tx pull -f -l hi
+echo "Dutch:"
+tx pull -f -l nl
 
 echo "Esperanto:"
 tx pull -f -l eo
 
 echo "English (United Kingdom):"
 tx pull -f -l en_GB
-
-echo "Greek:"
-tx pull -f -l el
-
-echo "Slovenian:"
-tx pull -f -l sl_SI
-
-echo "Spanish:"
-tx pull -f -l es
-
-echo "Spanish (Chile):"
-tx pull -f -l es_CL
-
-echo "Spanish (Argentina):"
-tx pull -f -l es_AR
-
-echo "Spanish (Peru):"
-tx pull -f -l es_PE
-
-echo "Basque:"
-tx pull -f -l eu
-
-echo "Persian:"
-tx pull -f -l fa
 
 echo "Finnish:"
 tx pull -f -l fi
@@ -66,14 +54,29 @@ tx pull -f -l fr
 echo "Galician:"
 tx pull -f -l gl
 
+echo "Georgian:"
+tx pull -f -l ka
+
+echo "German:"
+tx pull -f -l de
+
+echo "Greek:"
+tx pull -f -l el
+
 echo "Hebrew:"
 tx pull -f -l he
 
 echo "Hungarian:"
 tx pull -f -l hu_HU
 
-echo "Armenian:"
-tx pull -f -l hy
+echo "Hindi:"
+tx pull -f -l hi
+
+echo "Lithuanian:"
+tx pull -f -l lt
+
+echo "Persian:"
+tx pull -f -l fa
 
 echo "Indonesian (Indonesia):"
 tx pull -f -l id_ID
@@ -102,14 +105,20 @@ tx pull -f -l mk
 echo "Mongolian (Mongolia):"
 tx pull -f -l mn_MN
 
-echo "Dutch:"
-tx pull -f -l nl
-
 echo "Norwegian:"
 tx pull -f -l no
 
-echo "Occitan:"
+echo "Norwegian Bokmål:"
+tx pull -f -l nb
+
+echo "Occitan (post 1500):"
 tx pull -f -l oc
+
+echo "Persian:"
+tx pull -f -l fa
+
+echo "Panjabi (Punjabi):"
+tx pull -f -l pa
 
 echo "Polish:"
 tx pull -f -l pl
@@ -126,8 +135,32 @@ tx pull -f -l ro
 echo "Russian:"
 tx pull -f -l ru
 
+echo "Spanish:"
+tx pull -f -l es
+
+echo "Spanish (Argentina):"
+tx pull -f -l es_AR
+
+echo "Spanish (Chile):"
+tx pull -f -l es_CL
+
+echo "Spanish (Mexico)":
+tx pull -f -l ex_MX
+
+echo "Spanish (Paraguay):"
+tx pull -f -l ex_PY
+
+echo "Spanish (Peru):"
+tx pull -f -l es_PE
+
 echo "Serbian:"
 tx pull -f -l sr
+
+echo "Slovak:"
+tx pull -f -l sk
+
+echo "Slovenian:"
+tx pull -f -l sl_SI
 
 echo "Swahili:"
 tx pull -f -l sw
@@ -149,12 +182,3 @@ tx pull -f -l uk
 
 echo "Vietnamese:"
 tx pull -f -l vi
-
-echo "Chinese (China):"
-tx pull -f -l zh_CN
-
-echo "Chinese (Taiwan)"
-tx pull -f -l zh_TW
-
-echo "Chinese (Hong Kong)"
-tx pull -f -l zh_HK
