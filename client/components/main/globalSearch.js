0ff215f78f03c81d153dfc0ffa08fac94b542ec2
Lauri Ojansivu <x@xet7.org>
2021-01-28 17:51:35 +0200

    Try to fix quotes in Global Search.
    Thanks to xet7 !
    
    Related https://github.com/wekan/wekan/pull/3492

--- client/components/main/globalSearch.js
+++ client/components/main/globalSearch.js
@@ -190,10 +190,10 @@ BlazeComponent.extendComponent({
 
     this.searching.set(true);
 
-    const reOperator1 = /^((?<operator>[\p{Letter}\p{Mark}]+):|(?<abbrev>[#@]))(?<value>[\p{Letter}\p{Mark}]+)(\s+|$)/iu;
-    const reOperator2 = /^((?<operator>[\p{Letter}\p{Mark}]+):|(?<abbrev>[#@]))(?<quote>["']*)(?<value>.*?)\k<quote>(\s+|$)/iu;
-    const reText = /^(?<text>\S+)(\s+|$)/u;
-    const reQuotedText = /^(?<quote>["'])(?<text>[\w\p{L}]+)\k<quote>(\s+|$)/u;
+    const reOperator1 = `/^((?<operator>[\p{Letter}\p{Mark}]+):|(?<abbrev>[#@]))(?<value>[\p{Letter}\p{Mark}]+)(\s+|$)/iu`;
+    const reOperator2 = `/^((?<operator>[\p{Letter}\p{Mark}]+):|(?<abbrev>[#@]))(?<quote>["']*)(?<value>.*?)\k<quote>(\s+|$)/iu`;
+    const reText = `/^(?<text>\S+)(\s+|$)/u`;
+    const reQuotedText = `/^(?<quote>["'])(?<text>[\w\p{L}]+)\k<quote>(\s+|$)/u`;
 
     const operators = {
       'operator-board': 'boards',
