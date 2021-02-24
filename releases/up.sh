a43736b5c6196c65770d8ae17af927406dce2c43
Lauri Ojansivu <x@xet7.org>
2021-02-24 20:23:23 +0200

    Update wekan/releases/up.sh script: Can not build ppc64le version because OpenPower Minicloud is having emergency maintenance.
    Thanks to xet7 !

--- releases/up.sh
+++ releases/up.sh
@@ -2,8 +2,12 @@
 
 sudo apt-get -y install parallel
 
+# Can not build ppc64le version because
+# OpenPower Minicloud is having
+# emergency maintenance.
+#  ~/repos/wekan/releases/up-o.sh $1
+
 {
   ~/repos/wekan/releases/up-a.sh $1
   ~/repos/wekan/releases/up-s.sh $1
-  ~/repos/wekan/releases/up-o.sh $1
 } | parallel -k
