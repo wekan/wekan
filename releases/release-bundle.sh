799ae886c5fedad3bafa18a14f8fbbca7ad2c227
Lauri Ojansivu <x@xet7.org>
2021-02-24 19:37:51 +0200

    Update release-bundle.sh script: Can not build ppc64le version because OpenPower Minicloud is having emergency maintenance.
    Thanks to xet7 !

--- releases/release-bundle.sh
+++ releases/release-bundle.sh
@@ -5,13 +5,18 @@ sudo apt-get -y install parallel
 cd ~/repos/wekan/.build
 zip -r wekan-$1.zip bundle
 
+# Can not build ppc64le version because
+# OpenPower Minicloud is having
+# emergency maintenance.
+#   scp wekan-$1.zip o:/home/ubuntu/
+#   echo "Bundle uploaded to x2/a/s/o."
+
 {
   scp wekan-$1.zip x2:/var/snap/wekan/common/releases.wekan.team/
   scp wekan-$1.zip a:/home/wekan/repos/
   scp wekan-$1.zip s:/home/linux1/
-  scp wekan-$1.zip o:/home/ubuntu/
 } | parallel -k
 
 cd ..
 
-echo "Bundle uploaded to x2/a/s/o."
+echo "Bundle uploaded to x2/a/s."
