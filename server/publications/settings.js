71725f1b262b385162b2544f10658a0bc22f6b41
Lauri Ojansivu <x@xet7.org>
2021-02-25 09:02:23 +0200

    Fixed CRITICAL SECURITY ISSUE of SMTP password visible to Admin at Admin Panel by using browser inspect to see behind asterisks.
    Thanks to Georg Krause and xet7 !

--- server/publications/settings.js
+++ server/publications/settings.js
@@ -31,7 +31,18 @@ Meteor.publish('mailServer', function() {
   if (!Match.test(this.userId, String)) return [];
   const user = Users.findOne(this.userId);
   if (user && user.isAdmin) {
-    return Settings.find({}, { fields: { mailServer: 1 } });
+    return Settings.find(
+      {},
+      {
+        fields: {
+          'mailServer.host': 1,
+          'mailServer.port': 1,
+          'mailServer.username': 1,
+          'mailServer.enableTLS': 1,
+          'mailServer.from': 1,
+        },
+      },
+    );
   }
   return [];
 });
