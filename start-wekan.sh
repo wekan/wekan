4b9f8ab2b1dc822c013c25eea2e47119d62d44c5
Martin Filser <github-wekan@linux-email.de>
2021-06-09 19:54:49 +0200

    Adding DEFAULT_WAIT_SPINNER to all environments

--- start-wekan.sh
+++ start-wekan.sh
@@ -388,6 +388,9 @@
       #export SAML_LOCAL_PROFILE_MATCH_ATTRIBUTE=
       #export SAML_ATTRIBUTES=
       #---------------------------------------------------------------------
+      # Default wait spinner to use
+      #export DEFAULT_WAIT_SPINNER=Bounce
+      #---------------------------------------------------------------------
       node main.js
       # & >> ../../wekan.log
       cd ../..
