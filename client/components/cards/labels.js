94f8fb239348c621466046ce0e3c33238cfbe2b5
Martin Filser <github-wekan@linux-email.de>
2021-10-27 17:26:16 +0200

    Fix: Labels at desktop view with enabled drag handles were sortable

--- client/components/cards/labels.js
+++ client/components/cards/labels.js
@@ -67,7 +67,7 @@ BlazeComponent.extendComponent({
 
     // Disable drag-dropping if the current user is not a board member or is comment only
     this.autorun(() => {
-      if (Utils.isMiniScreen()) {
+      if (Utils.isMiniScreenOrShowDesktopDragHandles()) {
         $labels.sortable({
           handle: '.label-handle',
         });
