a67e3754ab33bfd6894403d8553141d9ed56dd6f
Lauri Ojansivu <x@xet7.org>
2021-11-15 16:35:59 +0200

    Updated translations.

--- releases/translations/pull-translations.sh
+++ releases/translations/pull-translations.sh
@@ -21,6 +21,9 @@ tx pull -f -l br
 echo "Catalan:"
 tx pull -f -l ca
 
+echo "Chinese:"
+tx pull -f -l zh
+
 echo "Chinese (China):"
 tx pull -f -l zh_CN
 
@@ -48,6 +51,9 @@ tx pull -f -l nl
 echo "Esperanto:"
 tx pull -f -l eo
 
+echo "English (Germany):"
+tx pull -f -l en_DE
+
 echo "English (Italy):"
 tx pull -f -l en_IT
 
@@ -72,11 +78,20 @@ tx pull -f -l ka
 echo "German:"
 tx pull -f -l de
 
+echo "German (Austria):"
+tx pull -f -l de_AT
+
 echo "German (Switzerland) => Deutsch (Schweiz):"
 tx pull -f -l de_CH
 
 echo "Greek:"
 tx pull -f -l el
+
+echo "Greek (Greece):"
+tx pull -f -l el_GR
+
+echo "Gurajati (India):"
+tx pull -f -l gu_IN
 
 echo "Hebrew:"
 tx pull -f -l he
@@ -86,6 +101,12 @@ tx pull -f -l hu_HU
 
 echo "Hindi:"
 tx pull -f -l hi
+
+echo "Hindi (India)":
+tx pull -f -l hi_IN
+
+echo "Khmer (km)":
+tx pull -f -l km
 
 echo "Lithuanian:"
 tx pull -f -l lt
@@ -119,6 +140,9 @@ tx pull -f -l lv_LV
 
 echo "Macedonian:"
 tx pull -f -l mk
+
+echo "Malay (Malaysia) (ms_MY):"
+ex pull -f -l ms_MY
 
 echo "Mongolian (Mongolia):"
 tx pull -f -l mn_MN
