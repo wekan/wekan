ebc7741fcb9ad854234921ed0546255411adeec9
Lauri Ojansivu <x@xet7.org>
2021-11-29 21:39:08 +0200

    Revert change from WeKan v5.81: At Sandstorm, every WeKan user is now WeKan Admin and has Admin Panel.
    Thank to ocdtrekkie and xet7 !
    
    Related #3423

--- sandstorm.js
+++ sandstorm.js
@@ -234,11 +234,7 @@ if (isSandstorm && Meteor.isServer) {
 
   function updateUserPermissions(userId, permissions) {
     const isActive = permissions.indexOf('participate') > -1;
-    // a) OLD 2021-11-29: First user is admin
-    //const isAdmin = permissions.indexOf('configure') > -1;
-    // b) NEW 2021-11-29: Every user is admin:
-    //    https://github.com/wekan/wekan/issues/3423
-    const isAdmin = true;
+    const isAdmin = permissions.indexOf('configure') > -1;
     const isCommentOnly = false;
     const isNoComments = false;
     const isWorker = false;
