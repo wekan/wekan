12448ab473c08a2c3c873d7ffa2b78e7630dc87a
Lauri Ojansivu <x@xet7.org>
2022-04-08 18:37:51 +0300

    Removed browser contect policy so that attachments would be visible. Part 2.
    Thanks to xet7 !
    
    Fixes #4468

--- server/policy.js
+++ server/policy.js
@@ -15,18 +15,18 @@ Meteor.startup(() => {
   if (process.env.BROWSER_POLICY_ENABLED === 'true') {
     // Trusted URL that can embed Wekan in iFrame.
     const trusted = process.env.TRUSTED_URL;
-    BrowserPolicy.framing.disallow();
+    ////BrowserPolicy.framing.disallow();
     //Allow inline scripts, otherwise there is errors in browser/inspect/console
     //BrowserPolicy.content.disallowInlineScripts();
     //BrowserPolicy.content.disallowEval();
     //BrowserPolicy.content.allowInlineStyles();
     //BrowserPolicy.content.allowFontDataUrl();
-    BrowserPolicy.framing.restrictToOrigin(trusted);
+    ////BrowserPolicy.framing.restrictToOrigin(trusted);
     //BrowserPolicy.content.allowScriptOrigin(trusted);
   } else {
     // Disable browser policy and allow all framing and including.
     // Use only at internal LAN, not at Internet.
-    BrowserPolicy.framing.allowAll();
+    ////BrowserPolicy.framing.allowAll();
   }
 
 
