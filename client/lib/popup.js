6eb1495c3bf9cb5d03391eb3690f2c77e1e8841d
Martin Filser <github-wekan@linux-email.de>
2022-04-11 20:15:12 +0200

    _getTopStack(), comment type error

--- client/lib/popup.js
+++ client/lib/popup.js
@@ -154,7 +154,7 @@ window.Popup = new (class {
     return BlazeComponent.getComponentForElement(openerElement);
   }
 
-  // An utility fonction that returns the top element of the internal stack
+  // An utility function that returns the top element of the internal stack
   _getTopStack() {
     return this._stack[this._stack.length - 1];
   }
