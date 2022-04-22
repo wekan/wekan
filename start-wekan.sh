50ce0ce442d804bf1d9eacacba3b493761ca6773
Lauri Ojansivu <x@xet7.org>
2022-04-22 16:18:48 +0300

    Added --trace-warnings to start-wekan.sh.
    Thanks to xet7 !

--- start-wekan.sh
+++ start-wekan.sh
@@ -508,7 +508,7 @@
       # Add more Node heap:
       export NODE_OPTIONS="--max_old_space_size=4096"
       # Add more stack:
-      bash -c "ulimit -s 65500; exec node --stack-size=65500 main.js"
+      bash -c "ulimit -s 65500; exec node --stack-size=65500 --trace-warnings main.js"
       #node main.js
       #---------------------------------------------------------------------
       # & >> ../../wekan.log
