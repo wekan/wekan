f67ffc910c7a44e69d799d2e5251c6cdb9bd88c1
Lauri Ojansivu <x@xet7.org>
2022-10-24 23:26:33 +0300

    Commented out ppc64el from release scripts.
    Thanks to xet7 !

--- releases/release-bundle.sh
+++ releases/release-bundle.sh
@@ -8,14 +8,14 @@ zip -r wekan-$1.zip bundle
 {
   scp ~/repos/wekan/releases/build-bundle-arm64.sh a:/home/wekan/
   scp ~/repos/wekan/releases/build-bundle-s390x.sh s:/home/linux1/
-  scp ~/repos/wekan/releases/build-bundle-ppc64el.sh o:/home/ubuntu/
+  #scp ~/repos/wekan/releases/build-bundle-ppc64el.sh o:/home/ubuntu/
   scp ~/repos/wekan/releases/release-x2.sh x2w:/var/websites/
   scp wekan-$1.zip x2w:/var/websites/releases.wekan.team/public/
   scp wekan-$1.zip a:/home/wekan/
   scp wekan-$1.zip s:/home/linux1/
-  scp wekan-$1.zip o:/home/ubuntu/
+  #scp wekan-$1.zip o:/home/ubuntu/
 } | parallel -k
 
 cd ..
 
-echo "x64 bundle and arm64/s390x/ppc64le build scripts uploaded to x2/a/s/o."
+echo "x64 bundle and arm64/s390x/ppc64le build scripts uploaded to x2/a/s."
