ccbc7f612295c7b20c0a2bcd912fa0d1102c1327
Lauri Ojansivu <x@xet7.org>
2022-12-20 03:21:12 +0200

    Fixed installing api2html when generating OpenAPI docs.
    Thanks to xet7 !

--- releases/rebuild-docs.sh
+++ releases/rebuild-docs.sh
@@ -16,7 +16,7 @@ fi
 if [ ! -d ~/python/esprima-python ]; then
   sudo apt-get -y install python3-pip
   pip3 install -U setuptools wheel
-  npm install -g api2html
+  sudo npm install -g api2html
   mkdir -p ~/python
   cd ~/python
   git clone --depth 1 -b master https://github.com/Kronuz/esprima-python
