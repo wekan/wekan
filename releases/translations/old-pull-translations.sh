bed2669ff8e136d4aedbd66bc2dfd73c38e18469
Lauri Ojansivu <x@xet7.org>
2023-01-30 14:15:22 +0200

    Updated translations.

---
+++ releases/translations/old-pull-translations.sh
@@ -0,0 +1,362 @@
+cd ~/repos/wekan
+
+echo "Arabic:"
+tx pull -f -l ar
+
+echo "Arabic (Egypt), simply Masri (مَصرى, [ˈmɑsˤɾi], Egyptian, Masr refers to Cairo):"
+tx pull -f -l ar_EG
+
+echo "Arabic (Moroccan):"
+tx pull -f -l ary
+
+echo "Arabic (Algeria):"
+tx pull -f -l ar_DZ
+
+echo "Azerbaijani:"
+tx pull -f -l az
+
+echo "Azerbaijani (Azerbaijan):"
+tx pull -f -l  az_AZ
+
+echo "Azerbaijani (Latin):"
+tx pull -f -l az@latin
+
+echo "Armenian:"
+tx pull -f -l hy
+
+echo "Basque:"
+tx pull -f -l eu
+
+echo "Bulgarian:"
+tx pull -f -l bg_BG
+
+echo "Breton:"
+tx pull -f -l br
+
+echo "Catalan:"
+tx pull -f -l ca
+
+echo "Chinese (China):"
+tx pull -f -l zh_CN
+
+echo "Chinese (Hong Kong):"
+tx pull -f -l zh_HK
+
+echo "Chinese Simplified (zh-Hans):"
+tx pull -f -l zh_Hans
+
+echo "Chinese (Taiwan):"
+tx pull -f -l zh_TW
+
+echo "Croatian:"
+tx pull -f -l hr
+
+echo "Czech:"
+tx pull -f -l cs
+
+echo "Czech (Czech Republic):"
+tx pull -f -l cs_CZ
+
+echo "Danish:"
+tx pull -f -l da
+
+echo "Dutch:"
+tx pull -f -l nl
+
+echo "Dutch (Netherlands):"
+tx pull -f -l nl_NL
+
+echo "Esperanto:"
+tx pull -f -l eo
+
+echo "Estonian (Estonia):"
+tx pull -f -l et_EE
+
+echo "English (Brazil):"
+tx pull -f -l en_BR
+
+echo "English (Germany):"
+tx pull -f -l en_DE
+
+echo "English (Italy):"
+tx pull -f -l en_IT
+
+echo "English (Malaysia) (en_MY):"
+tx pull -f -l en_MY
+
+echo "English (United Kingdom):"
+tx pull -f -l en_GB
+
+echo "Finnish:"
+tx pull -f -l fi
+
+echo "French:"
+tx pull -f -l fr
+
+echo "French (Switzerland):"
+tx pull -f -l fr_CH
+
+echo "Galician:"
+tx pull -f -l gl
+
+echo "Galician (Spain):"
+tx pull -f -l gl_ES
+
+echo "Georgian:"
+tx pull -f -l ka
+
+echo "German:"
+tx pull -f -l de
+
+echo "German (Austria):"
+tx pull -f -l de_AT
+
+echo "German (Switzerland) => Deutsch (Schweiz):"
+tx pull -f -l de_CH
+
+#echo "Greek:"
+#tx pull -f -l el
+
+echo "Greek (Greece):"
+tx pull -f -l el_GR
+
+echo "Gurajati (India):"
+tx pull -f -l gu_IN
+
+echo "Hebrew:"
+tx pull -f -l he
+
+echo "Hebrew (Israel):"
+tx pull -f -l he_IL
+
+echo "Hungarian:"
+tx pull -f -l hu_HU
+
+echo "Hindi:"
+tx pull -f -l hi
+
+echo "Hindi (India)":
+tx pull -f -l hi_IN
+
+echo "Khmer (km)":
+tx pull -f -l km
+
+echo "Lithuanian:"
+tx pull -f -l lt
+
+echo "Persian:"
+tx pull -f -l fa
+
+echo "Persian (Iran) فارسی/پارسی (ایران‎):"
+tx pull -f -l fa_IR
+
+echo "Indonesian (Indonesia):"
+tx pull -f -l id_ID
+
+echo "Igbo:"
+tx pull -f -l ig
+
+echo "Italian:"
+tx pull -f -l it
+
+echo "Japanese:"
+tx pull -f -l ja
+
+echo "Japanese (Hiragana):"
+tx pull -f -l ja-Hira
+
+echo "Khmer:"
+tx pull -f -l km
+
+echo "Korean:"
+tx pull -f -l ko
+
+echo "Korean (Korea):"
+tx pull -f -l ko_KR
+
+echo "Latvian (Latvia):"
+tx pull -f -l lv_LV
+
+echo "Macedonian:"
+tx pull -f -l mk
+
+echo "Malay (ms):"
+tx pull -f -l ms
+
+echo "Malay (Malaysia) (ms_MY):"
+tx pull -f -l ms_MY
+
+echo "Mongolian (Mongolia):"
+tx pull -f -l mn_MN
+
+#echo "Norwegian:"
+#tx pull -f -l no
+# This is same as no below
+
+echo "Norwegian Bokmål:"
+tx pull -f -l nb
+
+echo "Occitan (post 1500):"
+tx pull -f -l oc
+
+echo "Persian:"
+tx pull -f -l fa
+
+echo "Panjabi (Punjabi):"
+tx pull -f -l pa
+
+echo "Polish:"
+tx pull -f -l pl
+
+echo "Polish (Poland):"
+tx pull -f -l pl_PL
+
+echo "Portuguese:"
+tx pull -f -l pt
+
+echo "Portuguese (Brazil):"
+tx pull -f -l pt_BR
+
+echo "Romanian:"
+tx pull -f -l ro
+
+echo "Romanian (Romania):"
+tx pull -f -l ro_RO
+
+echo "Russian:"
+tx pull -f -l ru
+
+echo "Russian (Ukraine):"
+tx pull -f -l ru_UA
+
+echo "Spanish:"
+tx pull -f -l es
+
+echo "Spanish (Argentina):"
+tx pull -f -l es_AR
+
+echo "Spanish (Asturia):"
+tx pull -f -l ast_ES
+
+echo "Spanish (Chile):"
+tx pull -f -l es_CL
+
+echo "Spanish (Latin America)":
+tx pull -f -l es_419
+
+echo "Spanish (Mexico)":
+tx pull -f -l es_MX
+
+echo "Spanish (Paraguay):"
+tx pull -f -l es_PY
+
+echo "Spanish (Peru):"
+tx pull -f -l es_PE
+
+echo "Serbian:"
+tx pull -f -l sr
+
+echo "Slovak:"
+tx pull -f -l sk
+
+echo "Slovenian:"
+tx pull -f -l sl_SI
+
+echo "Standard Moroccan Tamazight"
+tx pull -f -l zgh
+
+echo "Swahili:"
+tx pull -f -l sw
+
+echo "Swedish:"
+tx pull -f -l sv
+
+echo "Tamil:"
+tx pull -f -l ta
+
+echo "Thai:"
+tx pull -f -l th
+
+echo "Turkish:"
+tx pull -f -l tr
+
+echo "Ukrainian:"
+tx pull -f -l uk
+
+echo "Ukrainian (Ukraine):"
+tx pull -f -l uk_UA
+
+echo "Uyghur (ug):"
+tx pull -f -l ug
+
+echo "Uzbek (uz):"
+tx pull -f -l uz
+
+echo "Uzbek (Arabic) (uz@Arab):"
+tx pull -f -l uz@Arab
+
+echo "Uzbek (Latin) (uz@Latn):"
+tx pull -f -l uz@Latn
+
+echo "Uzbek (Uzbekistan) (uz_UZ):"
+tx pull -f -l uz_UZ
+
+echo "Venda (ve):"
+tx pull -f -l ve
+
+echo "Venetian (vec):"
+tx pull -f -l vec
+
+echo "Vepsian (vep):"
+tx pull -f -l vep
+
+echo "Vietnamese:"
+tx pull -f -l vi
+
+echo "Vietnamese (Viet Nam) (vi_VN):"
+tx pull -f -l vi_VN
+
+echo "Vlaams (vls):"
+tx pull -f -l vls
+
+echo "Volapük (vo):"
+tx pull -f -l vo
+
+echo "Walloon (wa):"
+tx pull -f -l wa
+
+echo "Welsh (cy):"
+tx pull -f -l cy
+
+echo "Welsh (United Kingdom) (cy_GB):"
+tx pull -f -l cy_GB
+
+echo "Western Frisian (fy):"
+tx pull -f -l fy
+
+echo "Western Frisian (Netherlands) (fy_NL):"
+tx pull -f -l fy_NL
+
+echo "Wolof (wo):"
+tx pull -f -l wo
+
+echo "Wáray-Wáray (war):"
+tx pull -f -l war
+
+echo "Xhosa (xh):"
+tx pull -f -l xh
+
+echo "Yeshivish English (en@ysv):"
+tx pull -f -l en@ysv
+
+echo "Yiddish (yi):"
+tx pull -f -l yi
+
+echo "Yoruba (yo):"
+tx pull -f -l yo
+
+echo "Zulu (zu):"
+tx pull -f -l zu
+
+echo "Zulu (South Africa) (zu_ZA):"
+tx pull -f -l zu_ZA
