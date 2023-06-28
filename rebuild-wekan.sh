7d59ae93f9263c383a1c8c8605490d54b1e09ed7
Lauri Ojansivu <x@xet7.org>
2023-06-28 22:41:11 +0300

    Added Snap MONGO_URL to rebuild-wekan.sh dev options.
    Thanks to xet7 !

--- rebuild-wekan.sh
+++ rebuild-wekan.sh
@@ -161,6 +161,23 @@ do
 		break
 		;;
 
+    "Run Meteor for dev on http://CURRENT-IP-ADDRESS:4000 with MONGO_URL=mongodb://127.0.0.1:27019/wekan")
+                if [[ "$OSTYPE" == "darwin"* ]]; then
+                  IPADDRESS=$(ifconfig | grep broadcast | grep 'inet ' | cut -d: -f2 | awk '{ print $2}' | cut -d '/' -f 1 | grep '192.')
+                else
+                  IPADDRESS=$(ip a | grep 'noprefixroute' | grep 'inet ' | cut -d: -f2 | awk '{ print $2}' | cut -d '/' -f 1 | grep '192.')
+                fi
+                echo "Your IP address is $IPADDRESS"
+                #---------------------------------------------------------------------
+                #Not in use, could increase RAM usage: NODE_OPTIONS="--max_old_space_size=4096"
+                #---------------------------------------------------------------------
+                #Logging of terminal output to console and to ../wekan-log.txt at end of this line: 2>&1 | tee ../wekan-log.txt
+                #WARN_WHEN_USING_OLD_API=true NODE_OPTIONS="--trace-warnings"
+                MONGO_URL=mongodb://127.0.0.1:27019/wekan WRITABLE_PATH=.. WITH_API=true RICHER_CARD_COMMENT_EDITOR=false ROOT_URL=http://$IPADDRESS:4000 meteor run --exclude-archs web.browser.legacy,web.cordova --port 4000 2>&1 | tee ../wekan-log.txt
+                #---------------------------------------------------------------------
+                break
+                ;;
+
     "Run Meteor for dev on http://CUSTOM-IP-ADDRESS:PORT")
 		ip address
 		echo "From above list, what is your IP address?"
