ba9f9705d43189d2491266af1fd4817ff34a3b59
Lauri Ojansivu <x@xet7.org>
2023-06-30 18:06:44 +0300

    Show Meteor dependency chain with rebuild-wekan.sh .
    Thanks to xet7 !

--- rebuild-wekan.sh
+++ rebuild-wekan.sh
@@ -15,7 +15,7 @@ function pause(){
 
 echo
 PS3='Please enter your choice: '
-options=("Install Wekan dependencies" "Build Wekan" "Run Meteor for dev on http://localhost:4000" "Run Meteor for dev on http://localhost:4000 with trace warnings, and warnings using old Meteor API that will not exist in Meteor 3.0" "Run Meteor for dev on http://localhost:4000 with bundle visualizer" "Run Meteor for dev on http://CURRENT-IP-ADDRESS:4000" "Run Meteor for dev on http://CURRENT-IP-ADDRESS:4000 with MONGO_URL=mongodb://127.0.0.1:27019/wekan" "Run Meteor for dev on http://CUSTOM-IP-ADDRESS:PORT" "Quit")
+options=("Install Wekan dependencies" "Build Wekan" "Run Meteor for dev on http://localhost:4000" "Run Meteor for dev on http://localhost:4000 with trace warnings, and warnings using old Meteor API that will not exist in Meteor 3.0" "Run Meteor for dev on http://localhost:4000 with bundle visualizer" "Run Meteor for dev on http://CURRENT-IP-ADDRESS:4000" "Run Meteor for dev on http://CURRENT-IP-ADDRESS:4000 with MONGO_URL=mongodb://127.0.0.1:27019/wekan" "Run Meteor for dev on http://CUSTOM-IP-ADDRESS:PORT" "Show Meteor dependency chain" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -194,6 +194,12 @@ do
 		#---------------------------------------------------------------------
 		break
 		;;
+
+    "Show Meteor dependency chain")
+                meteor list --tree  | more
+                #---------------------------------------------------------------------
+                break
+                ;;
 
     "Quit")
 		break
