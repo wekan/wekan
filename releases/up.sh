73f943f89a89374a83cdfb31f0bfbfdfe4d6f52e
Lauri Ojansivu <x@xet7.org>
2023-08-18 02:00:12 +0300

    Fixed building s390x release.
    Thanks to xet7 !

--- releases/up.sh
+++ releases/up.sh
@@ -19,7 +19,7 @@ sudo apt-get -y install parallel
 
 {
   ~/repos/wekan/releases/up-a.sh $1
-  #~/repos/wekan/releases/up-s.sh $1
+  ~/repos/wekan/releases/up-s.sh $1
   #~/repos/wekan/releases/up-o.sh $1
   ~/repos/wekan/releases/up-w.sh $1
 } | parallel -k
