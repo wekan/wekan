8d9df72114f07aeded3b69916408cc8cce2b7f3d
helioguardabaxo <helio.guardabaxo@gmail.com>
2023-09-09 10:49:06 -0300

    Update Bootstrap Datepicker version to 1.10.0

---
+++ packages/wekan-bootstrap-datepicker/bootstrap-datepicker/js/locales/bootstrap-datepicker.ca.js
@@ -0,0 +1,18 @@
+/**
+ * Catalan translation for bootstrap-datepicker
+ * J. Garcia <jogaco.en@gmail.com>
+ */
+;(function($){
+	$.fn.datepicker.dates['ca'] = {
+		days: ["diumenge", "dilluns", "dimarts", "dimecres", "dijous", "divendres", "dissabte"],
+		daysShort: ["dg.",  "dl.", "dt.", "dc.", "dj.", "dv.", "ds."],
+		daysMin: ["dg", "dl", "dt", "dc", "dj", "dv", "ds"],
+		months: ["gener", "febrer", "març", "abril", "maig", "juny", "juliol", "agost", "setembre", "octubre", "novembre", "desembre"],
+		monthsShort: ["gen.", "febr.", "març", "abr.", "maig", "juny", "jul.", "ag.", "set.", "oct.", "nov.", "des."],
+		today: "Avui",
+		monthsTitle: "Mesos",
+		clear: "Esborra",
+		weekStart: 1,
+		format: "dd/mm/yyyy"
+	};
+}(jQuery));
