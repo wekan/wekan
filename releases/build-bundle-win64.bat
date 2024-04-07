802eeb1e6bb87c45fa79298f67ff690e87f939f8
Lauri Ojansivu <x@xet7.org>
2024-04-07 18:33:23 +0300

    Updated release script for Windows On-Premise, that is without container like Docker and Snap.
    Thanks to xet7 !

--- releases/build-bundle-win64.bat
+++ releases/build-bundle-win64.bat
@@ -1,26 +1,30 @@
 @ECHO OFF
 
+REM 1) Install newest Node.js and NPM version manager https://github.com/nodists/nodist/releases
+REM 2) nodist global 14.x
+REM 3) nodist npm global 6.
+REM 4) choco install -y 7zip curl wget git
+
 IF [%1] == [] GOTO usage
 
 ECHO 1) Deleting old bundle
 CALL DEL /F /S /Q bundle
 
 ECHO 2) Downloading new WeKan.zip
-CALL DEL wekan-%1-amd64.zip
-CALL wget https://releases.wekan.team/wekan-%1-amd64.zip --no-check-certificate
+DEL wekan-%1-amd64.zip
+wget https://releases.wekan.team/wekan-%1-amd64.zip --no-check-certificate
 
 ECHO 3) Unarchiving new WeKan
 CALL 7z x wekan-%1-amd64.zip
 
 ECHO 4) Reinstalling bcrypt
-REM cmd /c "npm -g install @mapbox/node-pre-gyp"
-REM cmd /c "npm -g install node-gyp"
-REM cmd /c "npm -g install fibers"
+cmd /c "npm -g install @mapbox/node-pre-gyp"
+cmd /c "npm -g install node-gyp"
+cmd /c "npm -g install fibers"
 CALL DEL /F /S /Q bundle\programs\server\npm\node_modules\meteor\accounts-password\node_modules\bcrypt
 cmd /c "CD bundle\programs\server\npm\node_modules\meteor\accounts-password && npm install bcrypt"
 REM # Sometimes may require building from source https://github.com/meteor/meteor/issues/11682
-REM # cmd /c "bundle\programs\server\npm\node_modules\meteor\accounts-password && npm rebuild --build-from-source && npm --build-from-source install bcrypt"
-REM # CD ..\..\..\..\..\..\..
+REM cmd /c "CD bundle\programs\server\npm\node_modules\meteor\accounts-password && npm rebuild --build-from-source && npm --build-from-source install bcrypt"
 
 ECHO 5) Packing new WeKan.zip
 CALL DEL wekan-%1-amd64-windows.zip
