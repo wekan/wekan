68c7a41906198df7e18917644a16b102c4507a93
Nadav Tasher <tashernadav@gmail.com>
2024-12-24 21:41:25 +0200

    Revert "Hide and disable Keyboard Shortcuts, because they make using Ctrl-C to copy etc impossible."
    This reverts commit 5606414f8975fa0f75642d2e3a6b48c7559186f9.

--- client/lib/keyboard.js
+++ client/lib/keyboard.js
@@ -1,6 +1,5 @@
 import { ReactiveCache } from '/imports/reactiveCache';
 
-/*
 // XXX There is no reason to define these shortcuts globally, they should be
 // attached to a template (most of them will go in the `board` template).
 
@@ -256,11 +255,8 @@ Mousetrap.bind('n', evt => {
     evt.preventDefault();
   }
 });
-*/
-
 
 Template.keyboardShortcuts.helpers({
-/*
   mapping: [
     {
       keys: ['w'],
@@ -323,5 +319,4 @@ Template.keyboardShortcuts.helpers({
       action: 'remove-labels-multiselect'
     },
   ],
-*/
 });
