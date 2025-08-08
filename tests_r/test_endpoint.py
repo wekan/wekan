c9ae1feddf1c8d3c6766cb04a3a53625a44e2539
rabeeafaraj <rabeea.30.03@gmail.com>
2025-08-08 16:41:21 +0300

    test_login solution

---
+++ tests_r/test_endpoint.py
@@ -0,0 +1,49 @@
+import unittest
+import requests
+
+class TestUserLogin(unittest.TestCase):
+    def test_user_login_success(self):
+        url = "http://localhost:80/users/login"
+        payload = {
+            "username": "rabeeaFaraj",
+            "password": "30fnhk03"
+        }
+        response = requests.post(url, json=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("token", response.json())
+
+    def test_user_login_wrong_password(self):
+        url = "http://localhost:80/users/login"
+        payload = {
+            "username": "rabeeaFaraj",
+            "password": "wrongpassword"
+        }
+        response = requests.post(url, json=payload)
+        self.assertEqual(response.status_code, 400)  # או 400 בהתאם למימוש
+        self.assertIn("error", response.json())
+
+    def test_user_login_missing_fields(self):
+        url = "http://localhost:80/users/login"
+        payload = {
+            "username": "rabeeaFaraj"
+            # חסר שדה סיסמה
+        }
+        response = requests.post(url, json=payload)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.json())
+
+    def test_user_login_nonexistent_user(self):
+        url = "http://localhost:80/users/login"
+        payload = {
+            "username": "notexist",
+            "password": "any"
+        }
+        response = requests.post(url, json=payload)
+
+        self.assertEqual(response.status_code, 400)  # או 404 בהתאם למימוש
+        self.assertIn("error", response.json())
+
+# ana btal
+if __name__ == "__main__":
+    unittest.main()
+
