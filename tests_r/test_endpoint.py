553d84ab208c66e899c9eec3f1991ec54af66f60
rabeeafaraj <rabeea.30.03@gmail.com>
2025-08-10 13:44:29 +0300

    bug fixes

--- tests_r/test_endpoint.py
+++ tests_r/test_endpoint.py
@@ -1,47 +1,51 @@
+import os
 import unittest
 import requests
 
 #test push
+WEKAN_URL = os.getenv("WEKAN_URL", "http://localhost:80/users/login")
+
 class TestUserLogin(unittest.TestCase):
+
     def test_user_login_success(self):
-        url = "http://localhost:80/users/login"
+
         payload = {
             "username": "rabeeaFaraj",
             "password": "30fnhk03"
         }
-        response = requests.post(url, json=payload)
+        response = requests.post(WEKAN_URL, json=payload)
         print("Status code:", response.status_code)
         print("Response JSON:", response.json())
         self.assertEqual(response.status_code, 200)
         self.assertIn("token", response.json())
 
     def test_user_login_wrong_password(self):
-        url = "http://localhost:80/users/login"
+
         payload = {
             "username": "rabeeaFaraj",
             "password": "wrongpassword"
         }
-        response = requests.post(url, json=payload)
+        response = requests.post(WEKAN_URL, json=payload)
         self.assertEqual(response.status_code, 400)  # או 400 בהתאם למימוש
         self.assertIn("error", response.json())
 
     def test_user_login_missing_fields(self):
-        url = "http://localhost:80/users/login"
+
         payload = {
             "username": "rabeeaFaraj"
             # חסר שדה סיסמה
         }
-        response = requests.post(url, json=payload)
+        response = requests.post(WEKAN_URL, json=payload)
         self.assertEqual(response.status_code, 400)
         self.assertIn("error", response.json())
 
     def test_user_login_nonexistent_user(self):
-        url = "http://localhost:80/users/login"
+
         payload = {
             "username": "notexist",
             "password": "any"
         }
-        response = requests.post(url, json=payload)
+        response = requests.post(WEKAN_URL, json=payload)
 
         self.assertEqual(response.status_code, 400)  # או 404 בהתאם למימוש
         self.assertIn("error", response.json())
