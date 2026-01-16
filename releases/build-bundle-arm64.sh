e2ec50730ff7fd4eb805071bb17fe0c105514f83
Lauri Ojansivu <x@xet7.org>
2026-01-16 22:14:07 +0200

    Update Linux arm64 bundle build script.
    Thanks to xet7 !

--- releases/build-bundle-arm64.sh
+++ releases/build-bundle-arm64.sh
@@ -10,39 +10,40 @@ if [ $# -ne 1 ]
     exit 1
 fi
 
-sudo apt -y install g++ build-essential p7zip-full npm
-sudo npm -g install n
-# Building bundle works with Node.js 14.21.3 .
-# Running works also with 14.21.4, many architectures at https://github.com/wekan/node-v14-esm/releases/tag/v14.21.4
-sudo n 14.21.3
+# Install deps
+sudo apt -y install g++ build-essential p7zip-full
 sudo npm -g uninstall node-pre-gyp
 sudo npm -g install @mapbox/node-pre-gyp
-rm -rf bundle
-rm wekan-$1-arm64.zip
-rm wekan-$1.zip
-wget https://github.com/wekan/wekan/releases/download/v$1/wekan-$1-amd64.zip
-7z x wekan-$1-arm64.zip
+# Remove old files
+rm -rf bundle 7.93
+rm wekan-$1-arm64.zip wekan-7.93-arm64.zip
 
-# Get working fibers and bcrypt from previous WeKan v7.93 https://github.com/wekan/wekan/releases/tag/v7.93
-wget https://github.com/wekan/wekan/releases/download/v7.93/wekan-7.93-arm64.zip
-mkdir 7.93
-cd 7.93
-7z x ../wekan-7.93-arm64.zip
-cd ..
+# Download newest WeKan, and WeKan v7.93 that has working fibers and bcrypt
+wget --no-check-certificate https://github.com/wekan/wekan/releases/download/v$1/wekan-$1-amd64.zip
+wget --no-check-certificate https://github.com/wekan/wekan/releases/download/v7.93/wekan-7.93-arm64.zip
 
-#wget https://releases.wekan.team/wekan-$1.zip
-#7z x wekan-$1-amd64.zip
+# Unarchive newest WeKan and WeKan v7.93
+7z x wekan-$1-amd64.zip
+(mkdir 7.93 && cd 7.93 && 7z x ../wekan-7.93-arm64.zip)
 
-#(cd bundle/programs/server && chmod u+w *.json && cd node_modules/fibers && node build.js)
-(cd bundle/programs/server && chmod u+w *.json)
-# && cd node_modules/fibers && node build.js)
+# Add working bcrypt
+rm -rf ~/repos/wekan/bundle/programs/server/npm/node_modules/meteor/accounts-password/node_modules/bcrypt
+cp -pR ~/repos/wekan/7.93/bundle/programs/server/npm/node_modules/meteor/accounts-password/node_modules/bcrypt \
+~/repos/wekan/bundle/programs/server/npm/node_modules/meteor/accounts-password/node_modules/
+
+# Add working fibers
+rm -rf ~/repos/wekan/bundle/programs/server/node_modules/fibers
+cp -pR ~/repos/wekan/7.93/bundle/programs/server/node_modules/fibers \
+~/repos/wekan/bundle/programs/server/node_modules/
+
+##(cd bundle/programs/server && chmod u+w *.json && cd node_modules/fibers && node build.js)
 #cd ../../../..
-(cd bundle/programs/server/npm/node_modules/meteor/accounts-password/node_modules && rm -rf bcrypt)
-(cp -pR 7.93/bundle/programs/server/npm/node_modules/meteor/accounts-password/node_modules/bcrypt bundle/programs/server/npm/node_modules/meteor/accounts-password/node_modules/)
+#(cd bundle/programs/server/npm/node_modules/meteor/accounts-password && npm remove bcrypt && npm install bcrypt)
 
 # Requires building from source https://github.com/meteor/meteor/issues/11682
-#(cd bundle/programs/server/npm/node_modules/meteor/accounts-password && npm rebuild --build-from-source)
+##(cd bundle/programs/server/npm/node_modules/meteor/accounts-password && npm rebuild --build-from-source)
 
+# Remove temporary files
 cd bundle
 find . -type d -name '*-garbage*' | xargs rm -rf
 find . -name '*phantom*' | xargs rm -rf
@@ -50,6 +51,7 @@ find . -name '.*.swp' | xargs rm -f
 find . -name '*.swp' | xargs rm -f
 cd ..
 
+# Make newest WeKan bundle for Linux arm64
 7z a wekan-$1-arm64.zip bundle
 
 #sudo snap start juju-db
