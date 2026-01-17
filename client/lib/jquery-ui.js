95da8966fe3bebc7c5ef2c1fc555de5fa239f8ca
Lauri Ojansivu <x@xet7.org>
2026-01-17 17:42:27 +0200

    Updated dependencies.
    Thanks to developers of dependencies !

--- client/lib/jquery-ui.js
+++ client/lib/jquery-ui.js
@@ -4,13 +4,10 @@ require('jquery-ui/ui/widget')
 require('jquery-ui/ui/scroll-parent')
 require('jquery-ui/ui/data')
 require('jquery-ui/ui/widgets/mouse')
-require('jquery-ui/ui/ie')
 require('jquery-ui/ui/widgets/sortable')
 
 // required for draggable
 require('jquery-ui/ui/plugin')
-require('jquery-ui/ui/safe-active-element')
-require('jquery-ui/ui/safe-blur')
 require('jquery-ui/ui/widgets/draggable')
 
 // everything already required for droppable
