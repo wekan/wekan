146905a459106b5d00b4f09453a6554255e6965a
Lauri Ojansivu <x@xet7.org>
2026-01-18 19:36:28 +0200

    Security Fix 6: SyncLDAPBleed.
    Thanks to [Joshua Rogers](https://joshua.hu) of [Aisle Research](https://aisle.com) and xet7.

--- packages/wekan-ldap/server/syncUser.js
+++ packages/wekan-ldap/server/syncUser.js
@@ -8,10 +8,9 @@ Meteor.methods({
       throw new Meteor.Error('error-invalid-user', 'Invalid user', { method: 'ldap_sync_users' });
     }
 
-    //TODO: This needs to be fixed - security issue -> alanning:meteor-roles
-    //if (!RocketChat.authz.hasRole(user._id, 'admin')) {
-    //	throw new Meteor.Error('error-not-authorized', 'Not authorized', { method: 'ldap_sync_users' });
-    //}
+    if (!user.isAdmin) {
+      throw new Meteor.Error('error-not-authorized', 'Not authorized', { method: 'ldap_sync_users' });
+    }
 
     if (LDAP.settings_get('LDAP_ENABLE') !== true) {
       throw new Meteor.Error('LDAP_disabled');
