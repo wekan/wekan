8d73d2f5751f24f7144ad09eb7205c57fcb85cd0
Lauri Ojansivu <x@xet7.org>
2026-05-10 01:25:38 +0300

    Release Scripts: Include updating version at same script.
    Thanks to xet7 !

--- releases/release.sh
+++ releases/release.sh
@@ -5,25 +5,27 @@
 # 1) Check that there is only one parameter
 #    of Wekan version number:
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
   then
-    echo "Syntax with Wekan version number:"
-    echo "  ./release.sh 5.10"
+    echo "Syntax with Wekan old and new version number:"
+    echo "  ./release.sh 9.16 9.17"
     exit 1
 fi
 
 # 2) Commit and push version number changes
 cd ~/repos/wekan
 
-~/repos/wekan/releases/release-bundle.sh $1
+~/repos/wekan/releases/version.sh $1 $2
+
+~/repos/wekan/releases/release-bundle.sh $2
 
 git add --all
 git add package-lock.json
-git commit -m "v$1"
+git commit -m "v$2"
 git push
 
 # 3) Add release tag
-~/repos/wekan/releases/add-tag.sh v$1
+~/repos/wekan/releases/add-tag.sh v$2
 
 # 4) Push to repo
 git push
@@ -31,7 +33,7 @@ git push
 # 5) Build Bundle
 
 # 6) Build Sandstorm
-#~/repos/wekan/releases/release-sandstorm.sh $1
+#~/repos/wekan/releases/release-sandstorm.sh $2
 
 # Build Snap
-#./release-snap.sh $1
+#./release-snap.sh $2
