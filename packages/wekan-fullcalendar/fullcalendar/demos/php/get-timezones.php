d0b2b5201e8a43a067ca32b0cf6a542695213406
Lauri Ojansivu <x@xet7.org>
2026-05-30 23:19:27 +0300

    Delete calendar demos, so that CodeQL stops complaining.
    Thanks to xet7 !

--- packages/wekan-fullcalendar/fullcalendar/demos/php/get-timezones.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//--------------------------------------------------------------------------------------------------
-// This script outputs a JSON array of all timezones (like "America/Chicago") that PHP supports.
-//
-// Requires PHP 5.2.0 or higher.
-//--------------------------------------------------------------------------------------------------
-
-echo json_encode(DateTimeZone::listIdentifiers());
