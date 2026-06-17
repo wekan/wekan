453fc3849fda18063e54428e503f3e0668af7214
Lauri Ojansivu <x@xet7.org>
2026-06-17 18:16:56 +0300

    - Fix Board Table view not loading with "No such template: tableView" error.   The `tableView.jade`, `tableView.js` and `tableView.css` files of the new Board   Table view were never imported in `client/features/boards.js`, so the `tableView`   template was not bundled and selecting the Table view threw a Tracker recompute   exception. Added the missing imports.
    Thanks to xet7.

--- client/features/boards.js
+++ client/features/boards.js
@@ -5,6 +5,7 @@ import '/client/components/boards/boardHeader.jade';
 import '/client/components/boards/boardsList.jade';
 import '/client/components/boards/miniboard.jade';
 import '/client/components/boards/originalPositionsView.jade';
+import '/client/components/boards/tableView.jade';
 
 import '/client/components/boards/boardArchive.js';
 import '/client/components/boards/boardBody.js';
@@ -12,6 +13,7 @@ import '/client/components/boards/dependencyOverlay.js';
 import '/client/components/boards/boardHeader.js';
 import '/client/components/boards/boardsList.js';
 import '/client/components/boards/originalPositionsView.js';
+import '/client/components/boards/tableView.js';
 
 import '/client/components/boards/boardBody.css';
 import '/client/components/boards/dependencyOverlay.css';
@@ -20,4 +22,5 @@ import '/client/components/boards/boardHeader.css';
 import '/client/components/boards/boardsList.css';
 import '/client/components/boards/calendarView.css';
 import '/client/components/boards/originalPositionsView.css';
+import '/client/components/boards/tableView.css';
 
