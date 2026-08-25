8dd33301c84056fdcef21cf9b85e70346ad7c3cf
Lauri Ojansivu <x@xet7.org>
2026-08-25 09:25:51 +0300

    Continue Central Kurdish Trello import translation

--- tests/centralKurdishTranslationProgress.test.cjs
+++ tests/centralKurdishTranslationProgress.test.cjs
@@ -11,7 +11,7 @@ const result = spawnSync(process.execPath, [fillScript, '--list', 'ckb'], {
 });
 assert.equal(result.status, 0, result.stderr);
 const remaining = JSON.parse(result.stdout);
-assert.equal(Object.keys(remaining).length, 1566);
+assert.equal(Object.keys(remaining).length, 1516);
 
 const english = JSON.parse(
   fs.readFileSync(path.join(root, 'imports/i18n/data/en.i18n.json'), 'utf8'),
@@ -142,3 +142,7 @@ assert.deepEqual(tokens(kurdish['import-board-instruction-issues']), [
 assert.match(kurdish['import-board-instruction-openproject'], /GET \/api\/v3\/work_packages/);
 assert.match(kurdish['import-board-instruction-jira'], /GET \/rest\/api\/2\/search/);
 assert.match(kurdish['import-trello-json-file-hint'], /API/);
+assert.match(kurdish['trello-api-key'], /https:\/\/trello\.com\/app-key/);
+assert.match(kurdish['trello-api-token'], /API/);
+assert.match(kurdish['invalid-year'], /2026/);
+assert.deepEqual(tokens(kurdish['label-default']), ['%s']);
