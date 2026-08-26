e3ab6e8de5c7713ba030f252e0b6f5274968ad24
Lauri Ojansivu <x@xet7.org>
2026-08-26 10:36:48 +0300

    Translate Manx voting and import strings.
    Thanks to xet7 !

--- tests/manxTranslationProgress.test.cjs
+++ tests/manxTranslationProgress.test.cjs
@@ -11,7 +11,7 @@ const result = spawnSync(process.execPath, [fillScript, '--list', 'gv'], {
 });
 assert.equal(result.status, 0, result.stderr);
 const remaining = JSON.parse(result.stdout);
-assert.equal(Object.keys(remaining).length, 1917);
+assert.equal(Object.keys(remaining).length, 1867);
 
 const english = JSON.parse(fs.readFileSync(path.join(root, 'imports/i18n/data/en.i18n.json'), 'utf8'));
 const manx = JSON.parse(fs.readFileSync(path.join(root, 'imports/i18n/data/gv.i18n.json'), 'utf8'));
