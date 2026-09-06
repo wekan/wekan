a1e4765930f270a1e844993c3ab9699c9a6cc4d4
Lauri Ojansivu <x@xet7.org>
2026-09-06 08:10:49 +0300

    Translate Tigrinya memory and checklist controls

--- tests/tigrinyaTranslationProgress.test.cjs
+++ tests/tigrinyaTranslationProgress.test.cjs
@@ -22,8 +22,8 @@ const fillResult = spawnSync(process.execPath, [
   'ti',
 ], { cwd: root, encoding: 'utf8' });
 assert.equal(fillResult.status, 0, fillResult.stderr);
-assert.equal(Object.keys(JSON.parse(fillResult.stdout)).length, 624,
-  'the first thirty-one Tigrinya batches stay resolved');
+assert.equal(Object.keys(JSON.parse(fillResult.stdout)).length, 574,
+  'the first thirty-two Tigrinya batches stay resolved');
 
 for (const [key, value] of Object.entries(tigrinya)) {
   if (value !== english[key]) {
@@ -250,5 +250,11 @@ assert.match(tigrinya['recovery-report-desc'], /MongoDB/);
 assert.match(tigrinya['office-report-desc'], /IPv4.*IPv6/);
 assert.equal(tigrinya['recovery-db'], 'ዳታቤዝ');
 assert.equal(tigrinya['history-change-moved'], 'ተዛዊሩ');
+assert.match(tigrinya['Node_heap_malloced_memory'], /malloc/);
+assert.match(tigrinya['custom-legal-notice-link-url'], /URL/);
+assert.equal(tigrinya.newLineNewItem,
+  'ሓደ መስመር ጽሑፍ = ሓደ ነገር ዝርዝር መረጋገጺ');
+assert.equal(tigrinya.copyChecklist, 'ዝርዝር መረጋገጺ ቅዳሕ');
+assert.equal(tigrinya['subtaskActionsPopup-title'], 'ተግባራት ንኡስ ዕማም');
 
-console.log('tigrinyaTranslationProgress: first thirty-one batches passed');
+console.log('tigrinyaTranslationProgress: first thirty-two batches passed');
