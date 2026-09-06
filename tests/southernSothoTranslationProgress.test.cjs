c63f9afb0722b0af647f76b40c23b4b954f4122c
Lauri Ojansivu <x@xet7.org>
2026-09-06 05:25:52 +0300

    Translate Southern Sotho list and selection controls

--- tests/southernSothoTranslationProgress.test.cjs
+++ tests/southernSothoTranslationProgress.test.cjs
@@ -22,8 +22,8 @@ const fillResult = spawnSync(process.execPath, [
   'st',
 ], { cwd: root, encoding: 'utf8' });
 assert.equal(fillResult.status, 0, fillResult.stderr);
-assert.equal(Object.keys(JSON.parse(fillResult.stdout)).length, 1524,
-  'the first twelve Southern Sotho batches stay resolved');
+assert.equal(Object.keys(JSON.parse(fillResult.stdout)).length, 1474,
+  'the first thirteen Southern Sotho batches stay resolved');
 
 for (const [key, value] of Object.entries(sesotho)) {
   if (value !== english[key]) {
@@ -96,5 +96,9 @@ assert.match(sesotho['trello-api-import-desc'], /Trello API/);
 assert.equal(sesotho['invalid-year'],
   'Selemo ha se sebetse. Ka kopo ngola dipalo tsohle tse nne, mohlala 2026.');
 assert.equal(sesotho['label-create'], 'Theha leibole');
+assert.deepEqual(tokens(sesotho['label-default']), ['%s']);
+assert.deepEqual(tokens(sesotho['leave-board-pop']), ['__boardTitle__']);
+assert.equal(sesotho.menu, 'Lenane la dikgetho');
+assert.equal(sesotho.normal, 'Tlwaelehileng');
 
-console.log('southernSothoTranslationProgress: first twelve batches passed');
+console.log('southernSothoTranslationProgress: first thirteen batches passed');
