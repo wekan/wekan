0ec7a74a90482204d5658ea2a8a7a55ad85a0f9b
Lauri Ojansivu <x@xet7.org>
2026-09-13 01:14:40 +0300

    Update mirrors

--- releases/mirror.sh
+++ releases/mirror.sh
@@ -30,5 +30,5 @@ mirror() {
 }
 
 mirror "gitlab" "git@gitlab.com:wekan/wekan"
-mirror "bitbucket" "git@bitbucket.org:wekan/wekan"
+mirror "bitbucket" "git@bitbucket.org:wekan/wekan.git"
 mirror "codeberg" "git@codeberg.org:wekan/wekan"
