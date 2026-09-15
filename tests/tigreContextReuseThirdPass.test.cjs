088b01587ffd781af8b177460ae9b2e3f6a93f27
Lauri Ojansivu <x@xet7.org>
2026-09-15 06:31:27 +0300

    Repair additional Tigre interface terms

---
+++ tests/tigreContextReuseThirdPass.test.cjs
@@ -0,0 +1,38 @@
+'use strict';
+
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+const root = path.resolve(__dirname, '..');
+const read = code =>
+  JSON.parse(
+    fs.readFileSync(
+      path.join(root, 'imports/i18n/data', code + '.i18n.json'),
+      'utf8',
+    ),
+  );
+const tigre = read('tig');
+const tigrinya = read('ti');
+const reviewed = {
+  'import-trello-zip-too-many-files':
+    'እቲ .zip ንምእታው ኣዝዮም ብዙሓት ፈይላት ኣለዉዎ።',
+  'uploading-files': 'ፈይላት ይስቀሉ ኣለዉ',
+  'email-addresses': 'ዕንዋንታት ኢመይል',
+  visibility: 'ርእየት',
+  'r-workflow-format': 'ሸክል',
+  'custom-field-stringtemplate-format':
+    'ሸክል (%{value} ከም መተካእታ ተጠቐም)',
+  filesReportTitle: 'ጸብጻብ ፈይላት',
+  'office-first-seen': 'መበገሲት ዝተራእየሉ',
+  'api-first-called': 'መበገሲት ዝተጸውዐሉ',
+  'sandstorm-disk-usage': 'መትነፍዒት ዲስክ',
+  'azure-container-description': 'ፈይላት ዝዕቀቡሉ blob container።',
+  'memory-usage': 'መትነፍዒት መዘክር',
+};
+for (const [key, value] of Object.entries(reviewed)) {
+  assert.equal(tigre[key], value, key + ': reviewed Tigre value');
+  assert.notEqual(tigre[key], tigrinya[key], key + ': Tigrinya seed removed');
+}
+assert.match(tigre['custom-field-stringtemplate-format'], /%\{value\}/);
+assert.match(tigre['import-trello-zip-too-many-files'], /\.zip/);
+console.log('Tigre third context-reuse pass: ' + Object.keys(reviewed).length);
