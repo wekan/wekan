e114ffde8984a3ac65e3622ca14701a49351e584
Lauri Ojansivu <x@xet7.org>
2026-09-15 09:22:50 +0300

    Replace Tamazight sky and metal-color seeds

--- tests/zghNativeColorTerms.test.cjs
+++ tests/zghNativeColorTerms.test.cjs
@@ -15,4 +15,13 @@ assert.equal(zgh['color-darkgreen'], 'ⴰⵣⴳⵣⴰ ⴰⴱⵔⴽⴰⵏ');
 assert.match(zgh['color-darkgreen'], new RegExp(zgh['color-green']));
 assert.match(zgh['color-darkgreen'], new RegExp(zgh['color-black']));
 assert.notEqual(zgh['color-darkgreen'], 'اخضر غامق');
+for (const [key, source, value, old] of [
+  ['color-sky', 'sky', 'ⵉⴳⵏⵏⴰ', 'ciel'],
+  ['color-gold', 'gold', 'ⵓⵔⵖ', 'ذهبي'],
+  ['color-silver', 'silver', 'ⴰⵥⵔⴼ', 'فضي'],
+]) {
+  assert.equal(en[key], source);
+  assert.equal(zgh[key], value);
+  assert.notEqual(zgh[key], old);
+}
 console.log('zghNativeColorTerms: pink and dark-green senses verified');
